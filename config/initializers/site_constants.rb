3f1f4ef73361a35be44fb92a1393de1a13a358ef
Jared Beck <jared@singlebrook.com>
2010-11-29 12:53:00 -0500

    define site constants in an initializer

---
+++ config/initializers/site_constants.rb
@@ -0,0 +1,6 @@
+# Application constants in alphabetical order
+# Changes here require server restart
+CONGRESS_CITY = "Santa Barbara"
+CONGRESS_DATE_RANGE = "Jul 30 - Aug 7"
+CONGRESS_STATE = "CA"
+CONGRESS_YEAR = "2011" # not always == Time.now.year
