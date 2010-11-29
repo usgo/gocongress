11a77413da9535d02c6220b9d280552e9fdc0f6d
Jared Beck <jared@jaredbeck.com>
2010-11-28 21:13:32 -0500

    IR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gocongress::Application.config.secret_token = '26ee2e9b7be913d4bc3b218b03422b89d0bd3476610f8a88716cef0ef1fb7d23a0c58e703203182fee666b99ec2afdb3c363e9ca6a9e06af6720c216f3903dbf'
