b8556fb4144205fb6296ff88bf9b00b3609a919b
Jared Beck <jared@singlebrook.com>
2010-12-20 18:28:40 -0500

    updated slideshow captions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,18 @@
 class HomeController < ApplicationController
 
 	def index
-		@bodyClassList = "homepage"
+		@bodyClassList = "homepage"		
 		@arSlideTitles = [ \
-			"UCSB Campus" \
-			, "This could be our tournament hall! (Corwin Pavilion)" \
-			, "University Center and Lagoon" \
-			, "UCSB Bike path" \
-			, "City and Mountains" \
-			, "Los Angeles?" \
-			, "Santa Barbara Mission" \
-			, "Beach and Mountains" \
-			, "UCSB?" \
-			]
+			"A view of the UCSB campus from the ocean" \
+			, "The main playing area: Corwin Pavillion, lying at the heart of campus, is steps away from the dorms, Youth room, dining hall, and the Lagoon" \
+			, "A view across the Lagoon of the University Center, housing the main playing area" \
+			, "Beautifully manicured bike paths provide access to campus facilities, as well as to nearby off-campus amenities" \
+			, "The view inland from campus" \
+			, "Santa Barbara city-scape from the courthouse tower" \
+			, "The historic Santa Barbara Mission" \
+			, "The beach at UCSB" \
+			, "Aerial view of the UCSB campus" \
+			]			
 	end
 
 end
