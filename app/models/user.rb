68aa15af8573ba157e96393d3144630b8831c061
Jared Beck <jared@singlebrook.com>
2010-12-20 21:30:54 -0500

    fix "can't assign jobs to users"

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,11 @@ class User < ActiveRecord::Base
 	devise :database_authenticatable, :registerable,
 				 :recoverable, :rememberable, :trackable, :validatable
 	
-	# Setup accessible (or protected) attributes for your model
-	attr_accessible :email, :password, :password_confirmation, :remember_me, :full_name, :is_admin
+	# Specify a white list of model attributes that can be set via mass-assignment
+	attr_accessible :email, :password, :password_confirmation, :remember_me, :full_name, :is_admin, :job_ids
 	
 	validates_presence_of :full_name
-
-	# Use validates_inclusion_of for booleans
 	validates_inclusion_of :is_admin, :in => [true, false]
-
-	# Devise seems to magically supply the following two validations -Jared
-	#validates_presence_of :email
-	#validates_uniqueness_of :email
 		
 	has_many :user_jobs
 	has_many :jobs, :through => :user_jobs
