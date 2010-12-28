0a17f379e2b4aea8fb0f8be99394d6c8a8e70649
Jared Beck <jared@jaredbeck.com>
2010-12-27 20:05:03 -0500

    raise_delivery_errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Gocongress::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
@@ -46,4 +46,5 @@ Gocongress::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
 end
