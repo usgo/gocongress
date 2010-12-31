40184562aa1f5eb28a28df5c68974245d677742f
Jared Beck <jared@jaredbeck.com>
2010-12-31 12:53:07 -0500

    i got tired of running export GMAIL_SMTP_USER in my shell

--- vendor/plugins/gmail_smtp/init.rb
+++ vendor/plugins/gmail_smtp/init.rb
@@ -1,2 +1,12 @@
+# In local dev, Gmail SMTP account will be defined in a file for convenience
+# Obviously, this file should never be committed to source control
+# Thus, at heroku, this file will not be present
+# For heroku, use heroku config:add GMAIL_SMTP_USER=username@gmail.com
+# -Jared 2010.12.31
+if File.file?("vendor/plugins/gmail_smtp/lib/usgc_gmail_account_info.rb") then
+	require 'usgc_gmail_account_info'
+end
+
+# from the original plugin
 require 'smtp_tls'
 require 'actionmailer_gmail'
