afb690816e458b150e9bff3e238fef47cdf9090b
Jared Beck <jared@jaredbeck.com>
2010-12-31 13:13:00 -0500

    fleshing out the "welcome email"

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,9 @@ class UserMailer < ActionMailer::Base
   
   def welcome_email(user)
 	  @user = user
+	  @host = default_url_options[:host]
+	  @port = default_url_options[:port]
+	  @lrFormUrl = "http://" + @host + ":" + @port.to_s + "/docs/USGC2011-Liability-Release-Form.doc"
     mail(:to => user.email,
          :subject => "Welcome to USGC")
   end
