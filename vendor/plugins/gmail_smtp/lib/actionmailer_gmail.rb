c58b7e43af4f5c74266be46fa643400411513cca
Jared Beck <jared@jaredbeck.com>
2010-12-31 12:51:59 -0500

    Assert that an SMTP user_name is defined

--- vendor/plugins/gmail_smtp/lib/actionmailer_gmail.rb
+++ vendor/plugins/gmail_smtp/lib/actionmailer_gmail.rb
@@ -6,3 +6,8 @@ ActionMailer::Base.smtp_settings = {
 	:user_name => ENV['GMAIL_SMTP_USER'],
 	:password => ENV['GMAIL_SMTP_PASSWORD'],
 }
+
+# Assert that an SMTP user_name is defined -Jared 2010.12.31
+if ( ActionMailer::Base.smtp_settings[:user_name].blank? ) then
+	raise "Assertion failed: SMTP user_name is not defined"
+end
