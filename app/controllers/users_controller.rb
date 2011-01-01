08cab28c04d2900ce54bfc70060f8e9c26ef2c8c
Alf Mikula <amikula@gmail.com>
2010-12-31 16:46:07 -0800

    Filtering out users without primary_attendee_id from attendee list, reversing primary_attendee relationship

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
   def index
-    @users = User.all
+    @users = User.where("primary_attendee_id is not null")
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,67 +1,67 @@
 class User < ActiveRecord::Base
 
-	# Include default devise modules. Others available are:
-	# :token_authenticatable, :confirmable, :lockable and :timeoutable
-	devise :database_authenticatable, :registerable,
-				 :recoverable, :rememberable, :trackable, :validatable
-	
-	# Specify a white list of model attributes that CAN be set via mass-assignment
-	attr_accessible :email, :password, :password_confirmation, :remember_me, :job_ids, :primary_attendee_attributes
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable, :lockable and :timeoutable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  
+  # Specify a white list of model attributes that CAN be set via mass-assignment
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :job_ids, :primary_attendee_attributes
 
-	# Specify a black list of model attributes that CAN'T be set via mass-assignment
-	# On an unrelated note, I added a db-level default value of false for is_admin
-	# -Jared 2010-12-30
-	attr_protected :is_admin
-		
-	has_many :user_jobs, :dependent => :destroy
-	has_many :jobs, :through => :user_jobs
+  # Specify a black list of model attributes that CAN'T be set via mass-assignment
+  # On an unrelated note, I added a db-level default value of false for is_admin
+  # -Jared 2010-12-30
+  attr_protected :is_admin
+    
+  has_many :user_jobs, :dependent => :destroy
+  has_many :jobs, :through => :user_jobs
 
-	# A user may register multiple people, eg. their family
-	# The primary attendee corresponds with the user themselves
-  has_one  :primary_attendee, :class_name => 'Attendee'
+  # A user may register multiple people, eg. their family
+  # The primary attendee corresponds with the user themselves
+  belongs_to  :primary_attendee, :class_name => 'Attendee'
   has_many :attendees, :dependent => :destroy
 
-	validates_uniqueness_of :email
+  validates_uniqueness_of :email
 	validates_inclusion_of :is_admin, :in => [true, false]
 
-	# There must always be at least one attendee -Jared
-	validates_presence_of :primary_attendee
-	validates_associated :primary_attendee
+  # There must always be at least one attendee -Jared
+  validates_presence_of :primary_attendee
+  validates_associated :primary_attendee
 
-	after_create :send_welcome_email
-	
-	# Both User and Attendee have an email column, and we don't want to ask the
-	# enduser to enter the same email twice when signing up -Jared 2010.12.31
-	before_validation :apply_user_email_to_primary_attendee, :on => :create
+  after_create :send_welcome_email
 
-	# Nested Attributes allow us to create forms for attributes of a parent
-	# object and its associations in one go with fields_for()
+  # Both User and Attendee have an email column, and we don't want to ask the
+  # enduser to enter the same email twice when signing up -Jared 2010.12.31
+  before_validation :apply_user_email_to_primary_attendee, :on => :create
+
+  # Nested Attributes allow us to create forms for attributes of a parent
+  # object and its associations in one go with fields_for()
   accepts_nested_attributes_for :primary_attendee
 
 private
 
-	# make sure you:
-	# export GMAIL_SMTP_USER=username@gmail.com
-	# export GMAIL_SMTP_PASSWORD=yourpassword
-	# for heroku, use: heroku config:add GMAIL_SMTP_USER=username@gmail.com
-	# see /vendor/plugins/gmail_smtp/lib/actionmailer_gmail.rb
-	# -Jared 2010.12.27
-	def send_welcome_email
-		UserMailer.welcome_email(self).deliver
-	end
+  # make sure you:
+  # export GMAIL_SMTP_USER=username@gmail.com
+  # export GMAIL_SMTP_PASSWORD=yourpassword
+  # for heroku, use: heroku config:add GMAIL_SMTP_USER=username@gmail.com
+  # see /vendor/plugins/gmail_smtp/lib/actionmailer_gmail.rb
+  # -Jared 2010.12.27
+  def send_welcome_email
+    UserMailer.welcome_email(self).deliver
+  end
 
-	def apply_user_email_to_primary_attendee
-		# If primary_attendee isn't here, I don't want a NoMethodError
-		# Instead, I'd rather get a RecordNotValid
-		if primary_attendee.present?
-			primary_attendee.email = self.email
-		end
-	end
+  def apply_user_email_to_primary_attendee
+    # If primary_attendee isn't here, I don't want a NoMethodError
+    # Instead, I'd rather get a RecordNotValid
+    if primary_attendee.present?
+      primary_attendee.email = self.email
+    end
+  end
 
 protected
 
-	def password_required?
-		!persisted? || password.present? || password_confirmation.present?
-	end
+  def password_required?
+    !persisted? || password.present? || password_confirmation.present?
+  end
 
 end
