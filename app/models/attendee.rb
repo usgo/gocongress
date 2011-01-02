47f8d9dcc06204be0ef0129a9ff8b55d6e54d68b
Jared Beck <jared@jaredbeck.com>
2011-01-02 16:58:57 -0500

    add validation for attendee rank

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,15 +1,23 @@
 class Attendee < ActiveRecord::Base
   belongs_to :user
 
+  # define constant array of rank hashes {integer rank, rank name}
+  RANKS = []
+  RANKS << [ "Non-player", 0]
+  109.downto(101).each {|r| RANKS << ["#{r-100} pro", r] }
+  8.downto(1).each {|r| RANKS << [ "#{r} dan", r] }
+  -1.downto(-30).each {|r| RANKS << ["#{-r} kyu", r] }
+
+  # define constant array of integer ranks
+  NUMERIC_RANK_LIST = []
+  Attendee::RANKS.each { |r| NUMERIC_RANK_LIST << r[1] }
+
   # TODO: Add Validation for gender
-  # TODO: Add Validation for rank
   
+  validates_presence_of :rank
+  validates_inclusion_of :rank, :in => NUMERIC_RANK_LIST
   validates_presence_of :email
   validates_presence_of :birth_date
   validates :understand_minor, :minor_agreement => true
 
-  RANKS = []
-  109.downto(101).each {|r| RANKS << ["#{r-100} pro", r] }
-  8.downto(1).each {|r| RANKS << [ "#{r} dan", r] }
-  -1.downto(-30).each {|r| RANKS << ["#{-r} kyu", r] }
 end
