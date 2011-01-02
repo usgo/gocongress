ff34ad44dc8f3c7ad777fa15f1f75750c08bcc27
Alf Mikula <amikula@gmail.com>
2011-01-02 15:25:14 -0800

    Adding comment to explain the :understand_minor validation

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -18,14 +18,15 @@ class Attendee < ActiveRecord::Base
   validates_inclusion_of :rank, :in => NUMERIC_RANK_LIST, :message => "is not a valid rank"
   validates_presence_of :email
   validates_presence_of :birth_date
+
+  # Use MinorAgreementValidator (found in lib/) to require that understand_minor
+  # be checked if the attendee will not be 18 before the first day of the Congress.
   validates :understand_minor, :minor_agreement => true
 
-	public
-		def get_rank_name
-			rank_name = ""
-			RANKS.each { |r| if (r[1] == self.rank) then rank_name = r[0] end	}
-			if rank_name.empty? then raise "assertion failed: invalid rank" end
-			return rank_name
-		end
-
+  def get_rank_name
+    rank_name = ""
+    RANKS.each { |r| if (r[1] == self.rank) then rank_name = r[0] end }
+    if rank_name.empty? then raise "assertion failed: invalid rank" end
+    return rank_name
+  end
 end
