2945c07270c60eb219af25124e86edb26e2aabf4
Jared Beck <jared@jaredbeck.com>
2011-01-02 19:12:29 -0500

    rough sorting

--- app/controllers/attendee_controller.rb
+++ app/controllers/attendee_controller.rb
@@ -1,7 +1,17 @@
 class AttendeeController < ApplicationController
 
   def index
-  	@attendees = Attendee.all
+    # by default, sort by rank (0 is non-player)
+    order_by_clause = "rank = 0, rank desc"
+
+    # if a sort order was specified, make sure it is not a SQL injection attack
+    valid_sortable_columns = %w[given_name family_name rank created_at]
+    if (valid_sortable_columns.include?(params[:sort])) then
+      order_by_clause = params[:sort]
+    end
+
+    # get all attendees
+    @attendees = Attendee.order(order_by_clause)
   end
 
 end
