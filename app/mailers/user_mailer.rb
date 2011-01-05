5227911551b8ae43f24810bc45df6fe593188968
Jared Beck <jared@jaredbeck.com>
2011-01-04 19:48:44 -0500

    new version of the welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,11 @@ class UserMailer < ActionMailer::Base
   default :from => "usgcwebsite@gmail.com"
   
   def welcome_email(user)
-	  @user = user
-	  @host = default_url_options[:host]
-	  @port = default_url_options[:port]
-	  @lrFormUrl = "http://" + @host + ":" + @port.to_s + "/docs/USGC2011-Liability-Release-Form.doc"
+    @host = default_url_options[:host]
+    @port = default_url_options[:port]
+    @lrFormUrl = "http://" + @host + ":" + @port.to_s + "/docs/USGC2011-Liability-Release-Form.doc"
     mail(:to => user.email,
-         :subject => "Welcome to USGC")
+         :subject => "Welcome to the " + CONGRESS_YEAR.to_s + " US Go Congress")
   end
   
 end
