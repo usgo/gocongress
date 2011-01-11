7397b8b80d484a0d608846d29c24d923f874e497
Jared Beck <jared@jaredbeck.com>
2011-01-11 18:20:30 -0500

    drier validations

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -36,10 +36,12 @@ class Attendee < ActiveRecord::Base
   # to do: validate that each user has exactly one primary attendee
 
   # only apply these validations on the baduk form page ("player info")
-  validates_numericality_of :congresses_attended, :on => :update, :if => :form_page_is_baduk?
-  validates_inclusion_of :is_player, :in => [true, false], :on => :update, :if => :form_page_is_baduk?
-  validates_inclusion_of :will_play_in_us_open, :in => [true, false], :on => :update, :if => :form_page_is_baduk?
-  validates_inclusion_of :is_current_aga_member, :in => [true, false], :on => :update, :if => :form_page_is_baduk?
+  with_options :on => :update, :if => :form_page_is_baduk? do |baduk_page_options|
+    baduk_page_options.validates_numericality_of :congresses_attended
+    baduk_page_options.validates_inclusion_of :is_player, :in => [true, false]
+    baduk_page_options.validates_inclusion_of :will_play_in_us_open, :in => [true, false]
+    baduk_page_options.validates_inclusion_of :is_current_aga_member, :in => [true, false]
+  end
 
   def form_page_is_baduk?
     @form_page == :baduk
