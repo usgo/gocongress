ff73ac98ae288e9543c47963e2fe1654b668fa29
Jared Beck <jared@jaredbeck.com>
2011-01-15 17:49:05 -0500

    tidy up

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,33 +7,18 @@ class JobsController < ApplicationController
   # GET /jobs.xml
   def index
     @jobs = Job.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @jobs }
-    end
   end
 
   # GET /jobs/1
   # GET /jobs/1.xml
   def show
     @job = Job.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @job }
-    end
   end
 
   # GET /jobs/new
   # GET /jobs/new.xml
   def new
     @job = Job.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @job }
-    end
   end
 
   # GET /jobs/1/edit
@@ -45,15 +30,10 @@ class JobsController < ApplicationController
   # POST /jobs.xml
   def create
     @job = Job.new(params[:job])
-
-    respond_to do |format|
-      if @job.save
-        format.html { redirect_to(@job, :notice => 'Job was successfully created.') }
-        format.xml  { render :xml => @job, :status => :created, :location => @job }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @job.errors, :status => :unprocessable_entity }
-      end
+    if @job.save
+      redirect_to @job, :notice => 'Job was successfully created.'
+    else
+      render 'new'
     end
   end
 
@@ -61,15 +41,10 @@ class JobsController < ApplicationController
   # PUT /jobs/1.xml
   def update
     @job = Job.find(params[:id])
-
-    respond_to do |format|
-      if @job.update_attributes(params[:job])
-        format.html { redirect_to(@job, :notice => 'Job was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @job.errors, :status => :unprocessable_entity }
-      end
+    if @job.update_attributes(params[:job])
+      redirect_to @job, :notice => 'Job was successfully updated.'
+    else
+      render 'edit'
     end
   end
 
@@ -78,10 +53,6 @@ class JobsController < ApplicationController
   def destroy
     @job = Job.find(params[:id])
     @job.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(jobs_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to jobs_url
   end
 end
