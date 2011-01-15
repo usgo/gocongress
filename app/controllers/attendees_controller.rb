d0549092f826b2b179f233e8c34efaca2bd082d5
Jared Beck <jared@jaredbeck.com>
2011-01-15 18:36:58 -0500

    display country on the attendees list

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -9,7 +9,7 @@ class AttendeesController < ApplicationController
     order_by_clause = "rank = 0, rank desc"
 
     # if a sort order was specified, make sure it is not a SQL injection attack
-    valid_sortable_columns = %w[given_name family_name rank created_at]
+    valid_sortable_columns = %w[given_name family_name rank created_at country]
     if (valid_sortable_columns.include?(params[:sort])) then
       order_by_clause = params[:sort]
     end
