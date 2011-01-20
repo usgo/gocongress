3345b97aab09ad43b60f1633c933e1516134aa65
Jared Beck <jared@jaredbeck.com>
2011-01-20 18:44:41 -0500

    use localization for DRYer field labels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+	def trl_attr_for_model ( modelname, attributename )
+		# see config/locales/en.yaml
+		translate "activerecord.attributes." + modelname + "." + attributename
+	end
+
 end
--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,2 +1,7 @@
 module TransactionsHelper
+
+	def trl_attr ( attributename )
+		trl_attr_for_model 'transaction', attributename
+	end
+
 end
