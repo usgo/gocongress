e71207656e99164edca5724a5a35beca902116ed
Jared Beck <jared@jaredbeck.com>
2011-01-20 01:23:18 -0500

    sale amount must be greater than zero

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,10 +5,15 @@ class Transaction < ActiveRecord::Base
 	validates_presence_of :user_id, :trantype, :amount, :gwtranid, :gwdate
 	validates_length_of :trantype, :is => 1
 	validates_numericality_of :amount, :gwtranid
+	validates_numericality_of :amount, :greater_than => 0, :if => :amount_must_be_positive?
 	validates_uniqueness_of :gwtranid
 	
 	# define constant array of trantypes
 	TRANTYPES = [['Sale', 'S']]
+
+  def amount_must_be_positive?
+    self.trantype == 'S'
+  end
 
   def get_trantype_name
   	trantype_name = ''
