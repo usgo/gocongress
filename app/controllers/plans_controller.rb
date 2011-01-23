701dfb8e389f7c7590980e7c80e2131d5c97bbaf
Jared Beck <jared@jaredbeck.com>
2011-01-22 22:31:15 -0500

    remove some unwanted scaffolding

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -4,36 +4,18 @@ class PlansController < ApplicationController
   before_filter :allow_only_admin, :except => [:index]
 
   # GET /plans
-  # GET /plans.xml
   def index
     @plans = Plan.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @plans }
-    end
   end
 
   # GET /plans/1
-  # GET /plans/1.xml
   def show
     @plan = Plan.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @plan }
-    end
   end
 
   # GET /plans/new
-  # GET /plans/new.xml
   def new
     @plan = Plan.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @plan }
-    end
   end
 
   # GET /plans/1/edit
@@ -42,46 +24,29 @@ class PlansController < ApplicationController
   end
 
   # POST /plans
-  # POST /plans.xml
   def create
     @plan = Plan.new(params[:plan])
-
-    respond_to do |format|
-      if @plan.save
-        format.html { redirect_to(@plan, :notice => 'Plan was successfully created.') }
-        format.xml  { render :xml => @plan, :status => :created, :location => @plan }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @plan.errors, :status => :unprocessable_entity }
-      end
+    if @plan.save
+      redirect_to plans_path, :notice => 'Plan was successfully created.'
+    else
+      render :action => "new"
     end
   end
 
   # PUT /plans/1
-  # PUT /plans/1.xml
   def update
     @plan = Plan.find(params[:id])
-
-    respond_to do |format|
-      if @plan.update_attributes(params[:plan])
-        format.html { redirect_to(@plan, :notice => 'Plan was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @plan.errors, :status => :unprocessable_entity }
-      end
+    if @plan.update_attributes(params[:plan])
+      redirect_to plans_path, :notice => 'Plan was successfully updated.'
+    else
+      render :action => "edit"
     end
   end
 
   # DELETE /plans/1
-  # DELETE /plans/1.xml
   def destroy
     @plan = Plan.find(params[:id])
     @plan.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(plans_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to plans_url
   end
 end
