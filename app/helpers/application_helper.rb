06554e14b6c97e896ca7d23ee5865f873e74f01b
Jared Beck <jared@jaredbeck.com>
2011-01-23 16:04:00 -0500

    show invitation unless signed in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  def anyone_signed_in?
+    # See /lib/devise/controllers/helpers.rb
+    signed_in?(nil)
+  end
+
 	def trl_attr_for_model ( modelname, attributename )
 		# see config/locales/en.yaml
 		translate "activerecord.attributes." + modelname + "." + attributename
