8182c1949277d9be70777b7b1a21520bf7b5b7f4
Jared Beck <jared@jaredbeck.com>
2011-01-28 00:32:53 -0500

    validate plan name length

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,6 +1,7 @@
 class Plan < ActiveRecord::Base
 
 validates_presence_of :name, :description, :price, :age_min
+validates_length_of :name, :maximum => 50
 validates_numericality_of :price, :age_min
 validates_numericality_of :age_max, :allow_nil => true
 
