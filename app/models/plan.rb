29c8d9e9a7f7d2438212f7959b8380ce95511820
Jared Beck <jared@jaredbeck.com>
2011-01-27 23:44:49 -0500

    validate model

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,2 +1,7 @@
 class Plan < ActiveRecord::Base
+
+validates_presence_of :name, :description, :price, :age_min
+validates_numericality_of :price, :age_min
+validates_numericality_of :age_max, :allow_nil => true
+
 end
