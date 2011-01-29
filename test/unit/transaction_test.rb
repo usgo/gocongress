96af9e35a4e6ffee1129cc11414ff8b357b522e4
Jared Beck <jared@jaredbeck.com>
2011-01-29 16:22:27 -0500

    add test sales amount can not be negative

--- test/unit/transaction_test.rb
+++ test/unit/transaction_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class TransactionTest < ActiveSupport::TestCase
+  setup do
+    @transaction = Factory.create(:transaction)
+  end
+
   test "factory is valid" do
     assert Factory.build(:transaction).valid?
+  end
+
+  test "sales amount can not be negative" do
+    @transaction.trantype = 'S'
+    @transaction.amount = -34
+    assert_equal false, @transaction.valid?
   end
 end
