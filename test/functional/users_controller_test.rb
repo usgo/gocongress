93a09468cfb06969c6b3eb193ceb60668575c346
Jared Beck <jared@jaredbeck.com>
2011-01-30 02:25:24 -0500

    on user destroy, all dependent attendees should also be destroyed

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -108,6 +108,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test "admin can destroy a user" do
     sign_in @admin_user
+    destroyed_user_id = @user.id
 
     assert_difference('User.count', -1) do
       delete :destroy, :id => @user.to_param
@@ -115,6 +116,9 @@ class UsersControllerTest < ActionController::TestCase
 
     assert_redirected_to users_path
     assert_equal 'User deleted', flash[:notice]
+
+    # dependent attendees should also be destroyed
+    assert_equal 0, Attendee.where(:user_id => destroyed_user_id).count
   end
 
   test "admin can assign jobs to any user" do
