7afea16941f8b76f13785a880f75a68cb8e6c696
Jared Beck <jared@jaredbeck.com>
2011-03-05 14:23:24 -0500

    test "destroying an attendee also destroys dependent AttendeePlans"

--- test/unit/attendee_test.rb
+++ test/unit/attendee_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class AttendeeTest < ActiveSupport::TestCase
   setup do
     @user = Factory.create(:user)
+    @plan = Factory.create(:plan)
   end
 
   test "factory is valid" do
@@ -13,6 +14,20 @@ class AttendeeTest < ActiveSupport::TestCase
     assert_match( /^[A-Z]{2}$/, @user.primary_attendee.country )
     update_success = @user.primary_attendee.update_attributes( { :country => 'United States' } )
     assert !update_success
+  end
+
+  test "destroying an attendee also destroys dependent AttendeePlans" do
+    a = Factory(:attendee, :user_id => @user.id)
+    a.plans << @plan
+
+    # when we destroy the attendee, we expect all dependent AttendeePlans to be destroyed
+    destroyed_attendee_id = a.id
+    assert_difference('AttendeePlan.count', -1) do
+      a.destroy
+    end
+
+    # double check
+    assert_equal 0, AttendeePlan.where(:attendee_id => destroyed_attendee_id).count
   end
 
 end
