9ec39c64866119e3d2e7d0c9d590319ef7db8d20
Jared Beck <jared@jaredbeck.com>
2011-03-09 00:36:26 -0500

    add failing tests for non-player pricing

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -5,14 +5,42 @@ class UserTest < ActiveSupport::TestCase
     assert Factory.build(:user).valid?
   end
 
-  test "a user with N attendee owes N * 375 dollars" do
+  test "user (playing) with N go-playing attendees owes N * 375 dollars" do
     u = Factory(:user)
+    u.primary_attendee.is_player = true
     num_extra_attendees = 1 + rand(3)
     1.upto(num_extra_attendees) { |a|
-      u.attendees << Factory(:attendee, :user_id => u.id)
+      u.attendees << Factory(:attendee, :user_id => u.id, :is_player => true)
       }
     assert_equal num_extra_attendees + 1, u.attendees.size
     assert_equal (num_extra_attendees + 1) * 375, u.get_invoice_total
+  end
+
+  test "user (non-playing) with N non-playing attendees owes N * 75 dollars" do
+    u = Factory(:user)
+    u.primary_attendee.is_player = false
+    num_extra_attendees = 1 + rand(3)
+    1.upto(num_extra_attendees) { |a|
+      u.attendees << Factory(:attendee, :user_id => u.id, :is_player => false)
+      }
+    assert_equal num_extra_attendees + 1, u.attendees.size
+    assert_equal (num_extra_attendees + 1) * 75, u.get_invoice_total
+  end
+
+  test "user (non-playing) with N players and M non-players owes N * 375 + M * 75 dollars" do
+    u = Factory(:user)
+    u.primary_attendee.is_player = false
+    num_players = 1 + rand(3)
+    num_non_players = 1 + rand(3)
+    1.upto(num_players) { |a|
+      u.attendees << Factory(:attendee, :user_id => u.id, :is_player => true)
+      }
+    1.upto(num_non_players) { |a|
+      u.attendees << Factory(:attendee, :user_id => u.id, :is_player => false)
+      }
+    assert_equal num_players + num_non_players + 1, u.attendees.size
+    expected_cost = (num_non_players + 1) * 75 + (num_players * 375)
+    assert_equal expected_cost, u.get_invoice_total
   end
 
   test "amount paid is sum of transactions" do
