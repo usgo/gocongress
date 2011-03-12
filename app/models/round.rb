f91b0bae6c659384f2bcadb4e15eee5b63ec3837
Jared Beck <jared@jaredbeck.com>
2011-03-11 21:48:45 -0500

    validates_date :round_start

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,4 +2,5 @@ class Round < ActiveRecord::Base
   attr_accessible :round_start
   attr_protected :created_at, :updated_at, :tournament_id
   belongs_to :tournament
+  validates_date :round_start, :after => Date.civil(2011, 1, 1)
 end
