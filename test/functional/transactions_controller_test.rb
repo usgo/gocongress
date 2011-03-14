c1d988d42abbb58a21df27cc9c42b7dfad1a610b
Jared Beck <jared@jaredbeck.com>
2011-03-13 23:28:35 -0400

    test "neither visitor nor non-admin user can create transaction"

--- test/functional/transactions_controller_test.rb
+++ test/functional/transactions_controller_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class TransactionsControllerTest < ActionController::TestCase
   setup do
-    @admin_user = Factory.create(:admin_user)
-    @transaction = Factory.create(:transaction)
+    @user = Factory.create :user
+    @admin_user = Factory.create :admin_user
+    @transaction = Factory.create :transaction
   end
 
   test "admin user can get edit form" do
@@ -11,4 +12,18 @@ class TransactionsControllerTest < ActionController::TestCase
     get :edit, :id => @transaction.to_param
     assert_response :success
   end
+
+  test "neither visitor nor non-admin user can create transaction" do
+    post_to_create_should_be_denied
+    sign_in @user
+    post_to_create_should_be_denied
+  end
+
+  def post_to_create_should_be_denied
+    assert_no_difference('Transaction.count') do
+      post :create, :transaction => @transaction.attributes
+    end
+    assert_response 403
+  end
+  
 end
