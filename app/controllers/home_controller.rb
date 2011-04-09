4da6b3e9f0b20b2d15fa1c64890c73137690e789
Jared Beck <jared@jaredbeck.com>
2011-04-09 12:13:40 -0400

    fix date logic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,7 +15,7 @@ class HomeController < ApplicationController
 			]
 
     # Get announcements for the homepage
-    contents_where_clause = "show_on_homepage = ? and (expires_at is null or expires_at < ?)"
+    contents_where_clause = "show_on_homepage = ? and (expires_at is null or expires_at > ?)"
     @contents = Content.where(contents_where_clause, true, Time.now).order("created_at desc")
 	end
 
