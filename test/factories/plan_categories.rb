331c4fadc8c78ca70a59932590755a455d5d5b16
Jared Beck <jared@jaredbeck.com>
2011-04-17 22:18:59 -0400

    fix PlanFlagValidator test

---
+++ test/factories/plan_categories.rb
@@ -0,0 +1,13 @@
+Factory.define :plan_category do |f|
+  f.sequence(:name) { |n| "Factory Plan Category #{n}" }
+  f.show_on_prices_page false
+  f.show_on_roomboard_page false
+end
+
+Factory.define :roomboard_category, :parent => :plan_category do |f|
+  f.show_on_roomboard_page true
+end
+
+Factory.define :prices_category, :parent => :plan_category do |f|
+  f.show_on_prices_page true
+end
--- test/unit/plan_test.rb
+++ test/unit/plan_test.rb
@@ -1,15 +1,29 @@
 require 'test_helper'
 
 class PlanTest < ActiveSupport::TestCase
+  setup do
+    @prices_category = Factory(:prices_category)
+    @roomboard_category = Factory(:roomboard_category)
+  end
+
   test "factory is valid" do
     assert Factory.build(:plan).valid?
   end
 
-  test "a plan with neither rooms nor meals is invalid" do
+  test "a roomboard plan with neither rooms nor meals is invalid" do
     p = Factory.build(:plan)
     p.has_rooms = false
     p.has_meals = false
+    p.plan_category_id = @roomboard_category.id
     assert_equal false, p.valid?
+  end
+
+  test "an extras plan with neither rooms nor meals is valid" do
+    p = Factory.build(:plan)
+    p.has_rooms = false
+    p.has_meals = false
+    p.plan_category_id = @prices_category.id
+    assert_equal true, p.valid?
   end
 
   test "a plan with rooms or meals or both is valid" do
