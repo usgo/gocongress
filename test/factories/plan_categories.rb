126684549cf2fd137d6ca2460f48a3224a71c4e5
Jared Beck <jared@jaredbeck.com>
2011-04-25 01:38:06 -0400

    update factory

--- test/factories/plan_categories.rb
+++ test/factories/plan_categories.rb
@@ -2,6 +2,7 @@ Factory.define :plan_category do |f|
   f.sequence(:name) { |n| "Factory Plan Category #{n}" }
   f.show_on_prices_page false
   f.show_on_roomboard_page false
+  f.show_on_reg_form false
 end
 
 Factory.define :roomboard_category, :parent => :plan_category do |f|
