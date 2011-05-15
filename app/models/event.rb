42011f60b51b8e1d9a024ad1e73b22acc21ed444
Jared Beck <jared@jaredbeck.com>
2011-05-15 00:49:44 -0400

    validate event name

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,4 @@
 class Event < ActiveRecord::Base
-	attr_accessible :evtname, :evtdeparttime, :evtstarttime, :evtprice, :evtdate
-	attr_protected :created_at, :updated_at
-  validates_presence_of :evtdate
+  attr_accessible :evtname, :evtdeparttime, :evtstarttime, :evtprice, :evtdate
+  validates_presence_of :evtdate, :evtname
 end
