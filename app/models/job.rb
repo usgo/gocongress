cd76a0624af6c83b2a50656eb40d744f6d481b54
Jared Beck <jared@jaredbeck.com>
2011-05-15 00:53:15 -0400

    validate jobname

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,7 @@
 class Job < ActiveRecord::Base
   attr_accessible :jobname, :email, :total_needed, :vacancies, :description
-  attr_protected :created_at, :updated_at
   has_many :user_jobs
   has_many :users, :through => :user_jobs
   validates_numericality_of :total_needed, :vacancies, :allow_nil => true
+  validates_presence_of :jobname
 end
