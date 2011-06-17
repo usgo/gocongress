3a91aee19d9055208b8559895452892b2564710e
Jared Beck <jared@jaredbeck.com>
2011-06-17 01:28:11 -0400

    clean up view nicely with a helper and i18n

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -1,2 +1,10 @@
 module TournamentsHelper
+
+  def link_to_destroy(tournament)
+    link_to 'Delete' \
+      , tournament \
+      , :confirm => I18n.translate('crud.confirm.tournament.destroy') \
+      , :method => :delete
+  end
+
 end
