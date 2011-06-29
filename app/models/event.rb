863ecd7db46da7cbf11c7fa5c44d4a613fe910db
Jared Beck <jared@jaredbeck.com>
2011-06-28 23:29:09 -0400

    validates_numericality_of :evtprice

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,10 @@ class Event < ActiveRecord::Base
 
   validates_presence_of :start, :evtname
   validates_length_of :notes, :maximum => 250
+
+  # Price should be numeric, but for some crazy reason, 
+  # evtprice is a string.  Hence the ugly :unless blank? option
+  validates_numericality_of :evtprice, \
+    :greater_than_or_equal_to => 0, :allow_nil => true, \
+    :unless => Proc.new { |e| e.evtprice.blank? }
 end
