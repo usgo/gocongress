ae68047065cc38744274a54441ca2b3aa593b376
Jared Beck <jared@jaredbeck.com>
2011-07-26 00:44:43 -0400

    new tests

--- test/functional/reports_controller_test.rb
+++ test/functional/reports_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ReportsControllerTest < ActionController::TestCase
   setup do
     @user = Factory.create(:user)
+    @staff = Factory.create(:staff)
     @admin = Factory.create(:admin_user)
   end
 
@@ -11,4 +12,17 @@ class ReportsControllerTest < ActionController::TestCase
     get :overdue_deposits, :format => 'csv'
     assert_response :success
   end
+
+  test "staff can get attendees report" do
+    sign_in @staff
+    get :attendees, :format => 'csv'
+    assert_response :success
+  end
+
+  test "user cannot get attendees report" do
+    sign_in @user
+    get :attendees, :format => 'csv'
+    assert_response 403
+  end
+
 end
