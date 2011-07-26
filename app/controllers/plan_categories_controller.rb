a5791dd38a084bce4539a6f5ef72eaa780367485
Jared Beck <jared@jaredbeck.com>
2011-07-25 21:30:30 -0400

    authorize plan categories with cancan

--- app/controllers/plan_categories_controller.rb
+++ app/controllers/plan_categories_controller.rb
@@ -1,87 +1,46 @@
 class PlanCategoriesController < ApplicationController
 
-  # Access Control
-  before_filter :allow_only_admin
+  load_and_authorize_resource
 
   # GET /plan_categories
-  # GET /plan_categories.xml
   def index
-    @plan_categories = PlanCategory.order :name
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @plan_categories }
-    end
+    @plan_categories = @plan_categories.order :name
   end
 
   # GET /plan_categories/1
-  # GET /plan_categories/1.xml
   def show
-    @plan_category = PlanCategory.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @plan_category }
-    end
   end
 
   # GET /plan_categories/new
-  # GET /plan_categories/new.xml
   def new
-    @plan_category = PlanCategory.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @plan_category }
-    end
   end
 
   # GET /plan_categories/1/edit
   def edit
-    @plan_category = PlanCategory.find(params[:id])
   end
 
   # POST /plan_categories
-  # POST /plan_categories.xml
   def create
-    @plan_category = PlanCategory.new(params[:plan_category])
-
-    respond_to do |format|
-      if @plan_category.save
-        format.html { redirect_to(@plan_category, :notice => 'Plan category was successfully created.') }
-        format.xml  { render :xml => @plan_category, :status => :created, :location => @plan_category }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @plan_category.errors, :status => :unprocessable_entity }
-      end
+    if @plan_category.save
+      redirect_to(@plan_category, :notice => 'Plan category was successfully created.')
+    else
+      render :action => "new"
     end
   end
 
   # PUT /plan_categories/1
-  # PUT /plan_categories/1.xml
   def update
-    @plan_category = PlanCategory.find(params[:id])
-
-    respond_to do |format|
-      if @plan_category.update_attributes(params[:plan_category])
-        format.html { redirect_to(@plan_category, :notice => 'Plan category was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @plan_category.errors, :status => :unprocessable_entity }
-      end
+    if @plan_category.update_attributes(params[:plan_category])
+      redirect_to(@plan_category, :notice => 'Plan category was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
 
   # DELETE /plan_categories/1
-  # DELETE /plan_categories/1.xml
   def destroy
-    @plan_category = PlanCategory.find(params[:id])
     @plan_category.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(plan_categories_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to plan_categories_url
   end
+
 end
---
+++ test/functional/plan_categories_controller_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+class PlanCategoriesControllerTest < ActionController::TestCase
+  setup do
+    @pc = Factory.create(:plan_category)
+    @user = Factory.create(:user)
+    @admin = Factory.create(:admin_user)
+  end
+
+  test "admin can get index" do
+    sign_in @admin
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:plan_categories)
+  end
+
+  test "user cannot get index" do
+    sign_in @user
+    get :index
+    assert_response 403
+  end
+
+  test "user cannot create" do
+    sign_in @user
+    post :create, :plan_category => @pc.attributes
+    assert_response 403
+  end
+  
+  test "admin can create" do
+    sign_in @admin
+    assert @pc.valid?, "factory is not valid"
+    assert_difference('PlanCategory.count', +1) do
+      post :create, :plan_category => @pc.attributes
+    end
+    assert_redirected_to plan_category_path(assigns(:plan_category))
+  end
+
+  test "user cannot destroy" do
+    sign_in @user
+    delete :destroy, :id => @pc.id
+    assert_response 403
+  end
+
+end
