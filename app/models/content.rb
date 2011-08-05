9ee4612d9d48f31d4b7ff976c3d142978bd5ae0b
Jared Beck <jared@jaredbeck.com>
2011-08-04 21:56:07 -0700

    oops

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,7 +1,6 @@
 class Content < ActiveRecord::Base
   validates_presence_of :subject, :body
   validates_length_of :subject, :maximum => 100
-  validates_length_of :body
   validates_inclusion_of :show_on_homepage, :in => [true, false]
   validates_datetime :expires_at, :allow_blank => true
 end
