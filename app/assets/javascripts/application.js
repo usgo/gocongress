6ceef05097e20c6dfcb6fc0ed7ae65291e426168
Jared Beck <jared@jaredbeck.com>
2011-10-10 23:11:07 -0400

    remove comment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,3 @@
-// FIXME: Tell people that this is a manifest file, real code should go into discrete files
-// FIXME: Tell people how Sprockets and CoffeeScript works
-//
-// TO DO: jquery and jquery_ujs
-//
 //= require ../../../vendor/assets/javascripts/jquery/jquery.min.js
 //= require ../../../vendor/assets/javascripts/jquery/jquery-ui.min.js
 //= require rails.js
