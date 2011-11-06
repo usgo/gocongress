893105f36b8131074f359e33f3c015ae0df7f9a0
Jared Beck <jared@jaredbeck.com>
2011-11-06 17:58:32 -0500

    update contact url in welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ActionMailer::Base
   default :from => "usgcwebsite@gmail.com"
   
   def welcome_email(user)
+    @user = user
     @host = default_url_options[:host]
     @port = default_url_options[:port]
     @lrFormUrl = "http://" + @host + ":" + @port.to_s + "/docs/USGC2011-Liability-Release-Form.doc"
