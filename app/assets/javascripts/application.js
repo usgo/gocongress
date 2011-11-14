4b7cc0e395986b7e37b493ce4d7b1cef98fa993b
Jared Beck <jared@jaredbeck.com>
2011-11-13 19:30:14 -0500

    cleanup comments in layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,11 @@
+// The following three files must be included in this exact order:
 //= require ../../../vendor/assets/javascripts/jquery/jquery.min.js
 //= require ../../../vendor/assets/javascripts/jquery/jquery-ui.min.js
 //= require rails.js
+
+// The order is less importent for the other files:
 //= require usgc.js
+
+/* TODO: We only need the galleria stuff on the homepage Hiding this on other
+pages could improve page load by 80ms or more -Jared 2011.2.3 */
 //= require ../../../vendor/assets/javascripts/galleria/galleria-1.2.4.min.js
