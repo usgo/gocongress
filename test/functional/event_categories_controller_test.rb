da5a1804c93a18602799de35f417bbfdcec0ccc1
Jared Beck <jared@jaredbeck.com>
2011-12-03 18:14:25 -0500

    readability

--- test/functional/event_categories_controller_test.rb
+++ test/functional/event_categories_controller_test.rb
@@ -7,7 +7,7 @@ class EventCategoriesControllerTest < ActionController::TestCase
 
   test "anyone can show" do
     get :show, :year => Time.now.year, :id => @ec
-    assert_equal false, assigns[:events_by_date].nil?
+    assert_not_nil assigns[:events_by_date]
     assert_response :success
   end
 
