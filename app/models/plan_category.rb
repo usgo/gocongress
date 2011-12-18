aabdbceb4fa5c6aa12aff0ae01df59f07b29b7ba
Jared Beck <jared@jaredbeck.com>
2011-12-18 05:36:47 -0500

    hopefully fix syntax bug for postgres 8.3

--- app/models/plan_category.rb
+++ app/models/plan_category.rb
@@ -4,9 +4,20 @@ class PlanCategory < ActiveRecord::Base
   validates_presence_of :name
   validates :description, :length => {maximum: 200}
 
+  def self.prefixed_column_list
+    all_columns = %w[id created_at description name updated_at year]
+    return all_columns.map{|c| "plan_categories.#{c}"}.join(",")
+  end
+
   def self.nonempty
-    joins(:plans) \
-      .group("plan_categories.id") \
+    # postgres 8.3 seems to require that the select clause match the group
+    # clause.  this is unfortunate, because now i have to enumerate the
+    # columns and if they ever change i have to update prefixed_column_list().
+    # in local development on postgres 9.1 i can select * and group by id
+    # with no problem.
+    return joins(:plans) \
+      .select(prefixed_column_list)
+      .group(prefixed_column_list) \
       .having("count(*) > 0")
   end
 
