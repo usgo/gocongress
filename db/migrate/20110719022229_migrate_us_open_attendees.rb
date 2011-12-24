6c606b0b79b9f7c8d761f9f92b912dab8a9cb15f
Jared Beck <jared@jaredbeck.com>
2011-12-23 20:20:45 -0500

    create us open if not present

--- db/migrate/20110719022229_migrate_us_open_attendees.rb
+++ db/migrate/20110719022229_migrate_us_open_attendees.rb
@@ -1,7 +1,19 @@
 class MigrateUsOpenAttendees < ActiveRecord::Migration
+  
+  # Faux model protects against validations which may be added in future
+  class Tournament < ActiveRecord::Base
+  end
+  
   def up
     usopen = Tournament.find_by_name 'US Open'
-    raise "unable to find us open" unless usopen.present?
+    unless usopen.present?
+      usopen = Tournament.create!(name:'US Open', 
+        eligible: 'Anyone',
+        description: 'The national championship',
+        directors: 'TBA',
+        openness: 'O',
+        location: 'TBA')
+    end
     execute <<-EOQ
       insert into attendee_tournaments (attendee_id, tournament_id)
       select id, #{usopen.id} from attendees
