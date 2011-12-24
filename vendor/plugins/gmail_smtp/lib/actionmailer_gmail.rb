84a16aaeb5f03e6f641292ab78d3bdf246607a9e
Jared Beck <jared@jaredbeck.com>
2011-12-23 20:42:35 -0500

    warn instead of raise

--- vendor/plugins/gmail_smtp/lib/actionmailer_gmail.rb
+++ vendor/plugins/gmail_smtp/lib/actionmailer_gmail.rb
@@ -1,9 +1,14 @@
-# Assert that GMAIL_SMTP_USER is defined in the ruby ENV -Jared
+# Warn if GMAIL_SMTP_USER is not defined in the ruby ENV.
+# We can't raise an error because "The app’s config vars are not available
+# in the environment during the slug compilation process." when Heroku runs
+# rake assets:precompile.  
+# http://devcenter.heroku.com/articles/rails31_heroku_cedar
 if ENV['GMAIL_SMTP_USER'].blank?
-	raise "Assertion failed: GMAIL_SMTP_USER is not defined in the ruby ENV"
+	Rails.logger.warn "Warning: GMAIL_SMTP_USER is not defined in the ruby ENV"
 end
 
-# from the original plugin ..
+# Detailed configuration for :smtp delivery method.
+# Taken from the original plugin ..
 ActionMailer::Base.smtp_settings = {
 	:address => "smtp.gmail.com",
 	:port => 587,
@@ -15,5 +20,5 @@ ActionMailer::Base.smtp_settings = {
 
 # Assert that an SMTP user_name is defined -Jared 2010.12.31
 if ( ActionMailer::Base.smtp_settings[:user_name].blank? ) then
-	raise "Assertion failed: SMTP user_name is not defined"
+	Rails.logger.warn "Warning: SMTP user_name is not defined"
 end
