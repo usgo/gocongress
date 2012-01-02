f9cdf866afab173a44dc48b9c5c564d970e121fd
Jared Beck <jared@jaredbeck.com>
2012-01-02 00:51:28 -0500

    test that "quantity cannot exceed available inventory"

--- test/unit/attendee_plan_test.rb
+++ test/unit/attendee_plan_test.rb
@@ -2,12 +2,15 @@ require 'test_helper'
 
 class AttendeePlanTest < ActiveSupport::TestCase
 
+  setup do
+    @user = Factory :user
+  end
+
   test "qty less than 1 is invalid" do
-    u = Factory(:user)
-    u.attendees << Factory(:attendee, :user_id => u.id)
+    @user.attendees << Factory(:attendee, :user_id => @user.id)
     p = Factory :plan, :max_quantity => 1
     ap = AttendeePlan.new \
-      :attendee_id => u.attendees.first \
+      :attendee_id => @user.attendees.first.id \
       , :plan_id => p.id \
       , :quantity => 0
     assert !ap.valid?
@@ -17,4 +20,17 @@ class AttendeePlanTest < ActiveSupport::TestCase
     assert ap.valid?
   end
 
+  test "quantity cannot exceed available inventory" do
+    a = @user.primary_attendee
+    p = Factory :plan, inventory: 42
+    a.attendee_plans.create plan_id: p.id, quantity: 43
+    assert_equal false, a.valid?
+  end
+
+  test "quantity can equal available inventory" do
+    a = @user.primary_attendee
+    p = Factory :plan, inventory: 42, max_quantity: 999
+    ap = AttendeePlan.create attendee_id: a.id, plan_id: p.id, quantity: 42
+    assert ap.valid?
+  end
 end
