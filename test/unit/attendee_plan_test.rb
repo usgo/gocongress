c14ddc9cf7874d76531662595fb2342a32b5277c
Jared Beck <jared@jaredbeck.com>
2012-01-02 01:00:06 -0500

    refactor test setup

--- test/unit/attendee_plan_test.rb
+++ test/unit/attendee_plan_test.rb
@@ -3,16 +3,13 @@ require 'test_helper'
 class AttendeePlanTest < ActiveSupport::TestCase
 
   setup do
-    @user = Factory :user
+    user = Factory :user
+    @atnd = user.primary_attendee
+    @plan = Factory :plan, inventory: 42, max_quantity: 999
   end
 
   test "qty less than 1 is invalid" do
-    @user.attendees << Factory(:attendee, :user_id => @user.id)
-    p = Factory :plan, :max_quantity => 1
-    ap = AttendeePlan.new \
-      :attendee_id => @user.attendees.first.id \
-      , :plan_id => p.id \
-      , :quantity => 0
+    ap = AttendeePlan.new attendee_id: @atnd.id, plan_id: @plan.id, quantity: 0
     assert !ap.valid?
     ap.quantity = -1
     assert !ap.valid?
@@ -21,16 +18,12 @@ class AttendeePlanTest < ActiveSupport::TestCase
   end
 
   test "quantity cannot exceed available inventory" do
-    a = @user.primary_attendee
-    p = Factory :plan, inventory: 42
-    a.attendee_plans.create plan_id: p.id, quantity: 43
-    assert_equal false, a.valid?
+    @atnd.attendee_plans.build plan_id: @plan.id, quantity: 43
+    assert_equal false, @atnd.valid?
   end
 
   test "quantity can equal available inventory" do
-    a = @user.primary_attendee
-    p = Factory :plan, inventory: 42, max_quantity: 999
-    ap = AttendeePlan.create attendee_id: a.id, plan_id: p.id, quantity: 42
-    assert ap.valid?
+    @atnd.attendee_plans.build plan_id: @plan.id, quantity: 42
+    assert @atnd.valid?
   end
 end
