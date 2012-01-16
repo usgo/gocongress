64f114e186e7e25a7d6fcb9fd9826cadcb89743c
Jared Beck <jared@jaredbeck.com>
2012-01-15 23:57:42 -0500

    test that it's invalid without attendee

--- test/unit/attendee_plan_test.rb
+++ test/unit/attendee_plan_test.rb
@@ -8,6 +8,11 @@ class AttendeePlanTest < ActiveSupport::TestCase
     @plan = Factory :plan, inventory: 42, max_quantity: 999
   end
 
+  test "invalid without attendee" do
+    ap = AttendeePlan.new plan_id: @plan.id, quantity: 0, year: @plan.year
+    assert !ap.valid?
+  end
+
   test "qty less than 1 is invalid" do
     ap = AttendeePlan.new attendee_id: @atnd.id, plan_id: @plan.id, quantity: 0
     assert !ap.valid?
