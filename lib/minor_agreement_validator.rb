750f91a6c9d8fe7c0bd409fa86c8530c68b7e54c
Jared Beck <jared@jaredbeck.com>
2012-01-29 16:45:31 -0500

    tweak message

--- lib/minor_agreement_validator.rb
+++ lib/minor_agreement_validator.rb
@@ -2,8 +2,8 @@ class MinorAgreementValidator < ActiveModel::EachValidator
 
   def validate_each(attendee, attribute, checkbox_value)
     unless checkbox_value || !attendee.birth_date || !attendee.minor?
-      msg = " - Because this attendee will not be 18 years of age on the first day of the Congress, you must agree to submit the Minor Agreement Form to the deputy registrar."
-      attendee.errors[attribute] << (options[:message] || msg)
+      msg = " - Because this attendee will not be 18 years of age on the first day of the Congress, you must agree to submit the Liability Release to the registrar."
+      attendee.errors["Liability Release"] << (options[:message] || msg)
     end
   end
 end
