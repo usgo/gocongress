e90f13a43662b7304dbdcdc6141a23a74a1c1688
Jared Beck <jared@jaredbeck.com>
2012-01-29 18:06:21 -0500

    defunct

--- db/seeds.rb
+++
@@ -1,7 +0,0 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-
-# In 2011, we seeded the events and jobs.  Those records were modified on
-# the live site, so the seeds in this file had become outdated.
-# In 2012, hopefully we won't need any seeds.
-# One can always create the initial admin account through the console.
