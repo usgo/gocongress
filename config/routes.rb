a8952a7210e4078608dced6d588abb580f205a58
Jared Beck <jared@jaredbeck.com>
2012-02-01 01:15:34 -0500

    get strict about format in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,71 +1,87 @@
 Gocongress::Application.routes.draw do
-  get "home/access_denied"
 
-  # kaboom is an intentional error to test airbrake
-  get "home/kaboom"
+  # It's time to get strict about format.  If someone requests a .php,
+  # that's a 404!  I'd like to add :format => false to all my routes,
+  # but it makes my routes file way too ugly, even if I use
+  # with_options().  This constraint has the same effect.
+  constraints :format => // do
 
-  # these routes support multiple years with a year scope
-  scope ":year" do
-    get 'contact' => 'user_jobs#index'
-    get 'costs' => 'plan_categories#index'
-    get 'pricing' => 'home#pricing'
+    get "home/access_denied"
 
-    devise_for :users, :controllers => { :registrations => "registrations" }
+    # kaboom is an intentional error to test airbrake
+    get "home/kaboom"
 
-    resources :content_categories, :contents, :discounts, :event_categories
-    resources :jobs, :plan_categories, :tournaments, :transactions
-    resources :events, :except => [:index]
-    resources :plans, :except => [:index]
+    # these routes support multiple years with a year scope
+    scope ":year" do
 
-    resources :attendees, :except => :edit do
-      collection do
-        get 'vip'
-      end
-      member do
-        get 'print_summary', :as => 'print_summary_for'
-        get 'print_badge', :as => 'print_badge_for'
-        
-        # Replace #edit with a route that supports multiple pages
-        get 'edit/:page', :action => :edit, :as => :edit
+      # :year must be numeric
+      constraints :year => /\d+/ do
 
-        # Because there are so many plans, we want to show one category at
-        # a time. Because we are batch-editing attendee_plan records, it
-        # does not make sense to use nested resources here.
-        get 'edit/plans/:plan_category_id', :action => :edit_plans, :as => :edit_plans_for
-        put 'edit/plans/:plan_category_id', :action => :update_plans, :as => :update_plans_for
-      end
-    end
+        get 'contact' => 'user_jobs#index'
+        get 'costs' => 'plan_categories#index'
+        get 'pricing' => 'home#pricing'
 
-    resources :users do
-      member do
-        get 'edit_email', :as => 'edit_email_for'
-        get 'edit_password', :as => 'edit_password_for'
-        get 'invoice'
-        get 'ledger'
-        get 'pay'
-        get 'print_cost_summary', :as => 'print_cost_summary_for'
-        get 'choose_attendee'
+        devise_for :users, :controllers => { :registrations => "registrations" }
 
-        # todo: attendees should probably be a nested resource of users
-        # see http://guides.rubyonrails.org/routing.html#nested-resources
-        get 'attendees/new' => 'attendees#new', :as => 'add_attendee_to'
-      end
-    end
+        resources :content_categories, :contents, :discounts, :event_categories
+        resources :jobs, :plan_categories, :tournaments, :transactions
+        resources :events, :except => [:index]
+        resources :plans, :except => [:index]
 
-    # reports (not a restful resource, but this is a nice compact syntax)
-    resource :reports, :only => [] do
-      get :attendees, :atn_badges_all, :atn_badges_ind, :atn_reg_sheets,
-        :emails, :events, :index, :invoices, :outstanding_balances,
-        :tournaments, :transactions, :user_invoices
-    end
+        resources :attendees, :except => :edit do
+          collection do
+            get 'vip'
+          end
+          member do
+            get 'print_summary', :as => 'print_summary_for'
+            get 'print_badge', :as => 'print_badge_for'
 
-  end
+            # Replace #edit with a route that supports multiple pages
+            get 'edit/:page', :action => :edit, :as => :edit
 
-  # deprecated routes: redirect with 301 status code
-  # these routes will be deleted eventually
-  get 'contact' => redirect('/2012/contact')
+            # Because there are so many plans, we want to show one category at
+            # a time. Because we are batch-editing attendee_plan records, it
+            # does not make sense to use nested resources here.
+            get 'edit/plans/:plan_category_id', :action => :edit_plans, :as => :edit_plans_for
+            put 'edit/plans/:plan_category_id', :action => :update_plans, :as => :update_plans_for
+          end
+        end
 
-  # these routes should come last
-  get ":year" => 'home#index', :as => :year
-  root :to => "home#index"
+        resources :users do
+          member do
+            get 'edit_email', :as => 'edit_email_for'
+            get 'edit_password', :as => 'edit_password_for'
+            get 'invoice'
+            get 'ledger'
+            get 'pay'
+            get 'print_cost_summary', :as => 'print_cost_summary_for'
+            get 'choose_attendee'
+
+            # todo: attendees should probably be a nested resource of users
+            # see http://guides.rubyonrails.org/routing.html#nested-resources
+            get 'attendees/new' => 'attendees#new', :as => 'add_attendee_to'
+          end
+        end
+
+        # reports (not a restful resource, but this is a nice compact syntax)
+        resource :reports, :only => [] do
+
+          get :atn_badges_all, :atn_badges_ind, :atn_reg_sheets,
+            :emails, :events, :index, :invoices, :outstanding_balances,
+            :tournaments, :user_invoices
+
+          # these two reports are the only routes in the entire app
+          # that allow the :format dynamic segment.
+          constraints :format => /(csv)?/ do
+            get :attendees, :transactions
+          end
+        end
+
+        root :to => 'home#index', :as => :year, :via => :get
+
+      end # end constraint
+    end # end scope
+  end # end constraint
+
+  root :to => "home#index", :via => :get
 end
