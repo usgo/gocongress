7a69d98f4ed9428bb88cf4abb65bfa6cbf32357c
Jared Beck <jared@jaredbeck.com>
2012-02-13 00:29:53 -0500

    comments only

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,16 +4,19 @@ class Plan < ActiveRecord::Base
 attr_accessible :name, :price, :age_min, :age_max, :description, \
   :inventory, :max_quantity, :plan_category_id
 
-# FIXME: in the controller, somehow year needs to get set 
+# FIXME: in the controller, somehow year needs to get set
 # before authorize! runs.  until then, year needs to be accessible.
 attr_accessible :year
+
+# Associations
+# ------------
 
 belongs_to :plan_category
 has_many :attendee_plans, :dependent => :destroy
 has_many :attendees, :through => :attendee_plans
 
-# Scopes
-scope :appropriate_for_age, lambda {|age| where("(age_min is null or age_min <= ?) and (age_max is null or age_max >= ?)", age, age)}
+# Validations
+# -----------
 
 validates_presence_of :name, :description, :price, :age_min, :plan_category_id
 validates_length_of :name, :maximum => 50
@@ -30,6 +33,14 @@ validates :inventory,
     :only_integer => true, :greater_than => 0, :allow_nil => true,
     :message => " should be greater than 0 or left blank if unlimited"
     }
+
+# Scopes
+# ------
+
+scope :appropriate_for_age, lambda {|age| where("(age_min is null or age_min <= ?) and (age_max is null or age_max >= ?)", age, age)}
+
+# Public Instance Methods
+# -----------------------
 
 def age_range_in_words
   if age_min == 0 && age_max.blank?
