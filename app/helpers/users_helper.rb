c394b31ea4bfc1e98aeca2d4f45e9555180c3e61
Jared Beck <jared@jaredbeck.com>
2012-02-20 21:57:04 -0500

    fix silly bug where i forgot string interpolation

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
     if signed_in? && current_user == user
       "My #{page}"
     elsif user.primary_attendee.present?
-      "user.full_name_possessive #{page}"
+      "#{user.full_name_possessive} #{page}"
     else
       page
     end
