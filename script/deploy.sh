86a1222a69e23075f8c595c7265fd41d8ed7ffba
Jared Beck <jared@jaredbeck.com>
2012-03-03 01:51:04 -0500

    maint. mode

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,13 @@
 echo "Deploy staging .."
+heroku maintenance:on --app gocongress-dev
 git push stage master
 heroku run rake db:migrate --app gocongress-dev
+heroku maintenance:off --app gocongress-dev
 
 echo "Deploy production .."
+heroku maintenance:on --app gocongress
 git push prod master
 heroku run rake db:migrate --app gocongress
+heroku maintenance:off --app gocongress
 
 echo "Deploy complete"
