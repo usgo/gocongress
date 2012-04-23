c46078091b730d4207ee900cd2fb8194b354afb4
Jared Beck <jared@jaredbeck.com>
2012-04-23 00:23:46 -0400

    cache less

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,13 @@ Spork.prefork do
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
   RSpec.configure do |config|
     config.use_transactional_fixtures = true
   end
 end
 
 Spork.each_run do
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  require "#{ Rails.root }/config/routes"
+  FactoryGirl.reload
 end
