beb7ce877abc558b774b0ff73c1e283f39d455f6
Jared Beck <jared@jaredbeck.com>
2012-05-31 23:42:32 -0400

    fix an IE9- layout bug in the slideshow

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,4 +11,4 @@
 
 /* TODO: We only need the galleria stuff on the homepage Hiding this on other
 pages could improve page load by 80ms or more -Jared 2011.2.3 */
-//= require galleria/galleria-1.2.6.min.js
+//= require galleria/galleria-1.2.7.min.js
