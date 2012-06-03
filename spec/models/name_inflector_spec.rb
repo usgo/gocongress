64ee7fbcb4b05bea31fbf2169fd75c991cb1c879
Jared Beck <jared@jaredbeck.com>
2012-06-02 22:17:34 -0400

    ws and comment only

--- spec/models/name_inflector_spec.rb
+++ spec/models/name_inflector_spec.rb
@@ -3,27 +3,34 @@ require "spec_helper"
 describe NameInflector do
   describe "capitalize_name" do
     subject { NameInflector }
+
     it "handles all caps" do
       subject.capitalize_name("DUDE").should == "Dude"
     end
+
     it "handles all lower case" do
       subject.capitalize_name("dude").should == "Dude"
     end
+
     it "capitalizes all parts of a hyphenated name" do
       subject.capitalize_name("harnett-hARgRoVE").should == "Harnett-Hargrove"
     end
+
     it "preserves the capitalization of certain English prepositions" do
       subject.capitalize_name("MacIntyre").should == "MacIntyre"
       subject.capitalize_name("Macintyre").should == "Macintyre"
       subject.capitalize_name("macintyre").should == "Macintyre"
       subject.capitalize_name("McIntyre").should == "McIntyre"
     end
+
     it "always capitalizes certain other English prepositions" do
       subject.capitalize_name("O'BRIAN").should == "O'Brian"
       subject.capitalize_name("o'brian").should == "O'Brian"
     end
 
-    # TODO: Implement proper capitalization of other, less common international names.
+    # In the future, we could implement proper capitalization of
+    # other, less common international names.
+    #
     # http://en.wikipedia.org/wiki/Capitalization#Compound_names
     #
     # it "preserves the capitalization of Dutch noun prepositions" do
