31dc51b2ac04cad2cc4f30171e83037afd38bee4
Jared Beck <jared@jaredbeck.com>
2012-06-10 03:26:52 -0400

    unnecessary

--- app/controllers/user_jobs_controller.rb
+++ app/controllers/user_jobs_controller.rb
@@ -1,10 +1,6 @@
 class UserJobsController < ApplicationController
   include YearlyController
 
-  # Access Control
-  before_filter :allow_only_admin, :except => [:index]
-
-  # GET /user_jobs
   def index
     user_jobs_this_year = UserJob.joins(:job).where(:jobs => {:year => @year.year})
     @user_jobs = user_jobs_this_year.order(:jobname)
