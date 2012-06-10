1137c160761d7a3ecf365d070e09075200d06838
Jared Beck <jared@jaredbeck.com>
2012-06-10 03:24:08 -0400

    show TDs on contact page

--- app/controllers/user_jobs_controller.rb
+++ app/controllers/user_jobs_controller.rb
@@ -8,6 +8,7 @@ class UserJobsController < ApplicationController
   def index
     user_jobs_this_year = UserJob.joins(:job).where(:jobs => {:year => @year.year})
     @user_jobs = user_jobs_this_year.order(:jobname)
+    @tournaments = Tournament.yr(@year).where('directors <> ?', 'TBD')
   end
 
 end
