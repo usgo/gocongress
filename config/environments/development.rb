2988c53040aa1f2189d4253ec277ea6981123595
Jared Beck <jared@jaredbeck.com>
2012-07-08 15:30:23 -0400

    organize

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,12 +6,12 @@ Gocongress::Application.configure do
   # since you don't have to restart the webserver when you make code changes.
   config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
-
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
