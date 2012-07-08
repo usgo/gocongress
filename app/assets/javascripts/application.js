b795f494702a0d0ca962425b794e685eb311a4ac
Jared Beck <jared@jaredbeck.com>
2012-07-08 19:10:29 -0400

    use a stripped-down jquery ui
    with only the tools we need

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,17 @@
-// The following three files are provided by the jquery-rails gem.
+// The following files are provided by the jquery-rails gem.
 // https://github.com/indirect/jquery-rails
-// They must be included in this exact order:
+// They must be included in this order:
 
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
+
+/* To speed up page load, we are using a custom jquery-ui, which
+only has autocomplete, datepicker, and dialog. -Jared 2012-07-09 */
+//= require jquery-ui-1.8.21.custom.min.js
 
 // The order is less importent for the other files:
 //= require usgc.js
 
-/* TODO: We only need the galleria stuff on the homepage Hiding this on other
-pages could improve page load by 80ms or more -Jared 2011.2.3 */
+/* TODO: We only need the galleria stuff on the homepage. Hiding
+this on other pages could improve page speed -Jared 2011.2.3 */
 //= require galleria/galleria-1.2.7.min.js
