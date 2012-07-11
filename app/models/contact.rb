0e8c9320cc6ed1bfeccfaae71a6528b855a2c586
Jared Beck <jared@jaredbeck.com>
2012-07-11 00:45:03 -0400

    allow blank email, add maximum lengths

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,14 +2,23 @@ class Contact < ActiveRecord::Base
   include YearlyModel
   attr_accessible :email, :family_name, :given_name, :list_order,
     :phone, :title, :year
-  validates :email, \
-    :presence => true, \
-    :uniqueness => { :scope => :year, :case_sensitive => false }, \
-    :format => { :with => EMAIL_REGEX }
-  validates :family_name, :presence => true
-  validates :given_name, :presence => true
-  validates :list_order, :presence => true, :numericality => { :only_integer => true }
-  validates :title, :presence => true
+  validates :email,
+    :uniqueness => { :scope => :year, :case_sensitive => false },
+    :format => { :with => EMAIL_REGEX },
+    :length => { :maximum => 100 },
+    :allow_blank => true
+  validates :family_name,
+    :presence => true,
+    :length => { :maximum => 50 }
+  validates :given_name,
+    :presence => true,
+    :length => { :maximum => 50 }
+  validates :list_order,
+    :presence => true,
+    :numericality => { :only_integer => true }
+  validates :title,
+    :presence => true,
+    :length => { :maximum => 50 }
 
   def full_name
     given_name + " " + family_name
