b953b842bb1c18746e7ead112562e820e1b4ed68
Jared Beck <jared@jaredbeck.com>
2012-07-15 02:27:19 -0400

    remove tests replaced by shared examples

--- test/functional/transactions_controller_test.rb
+++ test/functional/transactions_controller_test.rb
@@ -11,18 +11,6 @@ class TransactionsControllerTest < ActionController::TestCase
     @year = Time.now.year
   end
 
-  test "admin can index transactions" do
-    sign_in @admin
-    get :index, :year => @year
-    assert_response :success
-  end
-
-  test "admin user can get edit form" do
-    sign_in @admin
-    get :edit, :id => @transaction.to_param, :year => @year
-    assert_response :success
-  end
-
   test "admin can create transaction" do
     sign_in @admin
 
@@ -34,20 +22,6 @@ class TransactionsControllerTest < ActionController::TestCase
       post :create, :transaction => t.attributes, :user_email => @user.email, :year => @year
     end
     assert_redirected_to transaction_path(assigns(:transaction))
-  end
-
-  test "staff can index transactions" do
-    sign_in @staff
-    get :index, :year => @year
-    assert_response :success
-  end
-
-  test "guest and user and staff cannot create transaction" do
-    post_to_create_should_be_denied
-    sign_in @user
-    post_to_create_should_be_denied
-    sign_in @staff
-    post_to_create_should_be_denied
   end
 
   test "admin can update transaction" do
@@ -68,30 +42,7 @@ class TransactionsControllerTest < ActionController::TestCase
     assert_response 403
   end
 
-  test "admin can destroy transaction" do
-    sign_in @admin
-    assert_difference('Transaction.count', -1) do
-      delete :destroy, :id => @transaction.id, :year => @year
-    end
-    assert_redirected_to transactions_path
-  end
-
-  test "guest and user and staff cannot destroy transaction" do
-    delete_transaction_should_be_denied
-    sign_in @user
-    delete_transaction_should_be_denied
-    sign_in @staff
-    delete_transaction_should_be_denied
-  end
-
 private
-
-  def post_to_create_should_be_denied
-    assert_no_difference('Transaction.count') do
-      post :create, :transaction => @transaction.attributes, :year => @year
-    end
-    assert_response 403
-  end
 
   def put_to_update(delta_amount, expected_diff)
     amount_before = Transaction.find(@transaction.id).amount
@@ -104,13 +55,6 @@ private
     amount_after = Transaction.find(@transaction.id).amount
     actual_diff = amount_after - amount_before
     assert (actual_diff - expected_diff).abs < 0.0001, "unexpected change in amount"
-  end
-
-  def delete_transaction_should_be_denied
-    assert_no_difference('Transaction.count') do
-      delete :destroy, :id => @transaction.id, :year => @year
-    end
-    assert_response 403
   end
 
 end
