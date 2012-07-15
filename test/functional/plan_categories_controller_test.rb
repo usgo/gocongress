88cc13d193756d906b749ec9c2c885255161eb1a
Jared Beck <jared@jaredbeck.com>
2012-07-15 16:46:00 -0400

    remove tests replaced by specs

--- test/functional/plan_categories_controller_test.rb
+++ test/functional/plan_categories_controller_test.rb
@@ -3,8 +3,6 @@ require 'test_helper'
 class PlanCategoriesControllerTest < ActionController::TestCase
   setup do
     @pc = FactoryGirl.create(:plan_category)
-    @user = FactoryGirl.create(:user)
-    @admin = FactoryGirl.create(:admin)
     @year = Time.now.year
   end
 
@@ -19,45 +17,4 @@ class PlanCategoriesControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:plan_category)
   end
-
-  test "admin can show" do
-    sign_in @admin
-    get :show, id: @pc.id, year: @year
-    assert_response :success
-    assert_not_nil assigns(:plan_category)
-  end
-
-  test "admin can get index" do
-    sign_in @admin
-    get :index, :year => @year
-    assert_response :success
-  end
-
-  test "admin can edit" do
-    sign_in @admin
-    get :edit, id: @pc.id, year: @year
-    assert_response :success
-  end
-
-  test "user cannot create" do
-    sign_in @user
-    post :create, :plan_category => @pc.attributes, :year => @year
-    assert_response 403
-  end
-
-  test "admin can create" do
-    sign_in @admin
-    new_category = FactoryGirl.build :plan_category
-    assert_difference('PlanCategory.count', +1) do
-      post :create, :plan_category => new_category.attributes, :year => @year
-    end
-    assert_redirected_to plan_category_path(assigns(:plan_category))
-  end
-
-  test "user cannot destroy" do
-    sign_in @user
-    delete :destroy, :id => @pc.id, :year => @year
-    assert_response 403
-  end
-
 end
