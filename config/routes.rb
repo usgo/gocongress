0814f24055ef551c308c776749116476c34955c3
Jared Beck <jared@jaredbeck.com>
2012-07-20 01:08:20 -0400

    add placeholder view for activities_controller#index
    just for consistency

--- config/routes.rb
+++ config/routes.rb
@@ -28,10 +28,9 @@ Gocongress::Application.routes.draw do
 
         devise_for :users, :controllers => { :registrations => "registrations" }
 
-        resources :contacts, :content_categories, :contents,
+        resources :activities, :contacts, :content_categories, :contents,
           :discounts, :activity_categories, :plan_categories,
           :tournaments, :transactions
-        resources :activities, :except => [:index]
         resources :attendee_statistics, :only => :index
         resources :plans, :except => [:index]
 
