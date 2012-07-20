879f6981d1e94883c86452d7bbe486a156754636
Jared Beck <jared@jaredbeck.com>
2012-07-20 01:12:44 -0400

    replace tests with specs

--- test/functional/activities_controller_test.rb
+++ test/functional/activities_controller_test.rb
@@ -4,7 +4,6 @@ class ActivitiesControllerTest < ActionController::TestCase
   setup do
     @activity = FactoryGirl.create(:activity)
     @user = FactoryGirl.create(:user)
-    @admin_user = FactoryGirl.create(:admin)
   end
 
   test "user cannot get new" do
@@ -17,15 +16,6 @@ class ActivitiesControllerTest < ActionController::TestCase
     sign_in @user
     post :create, :year => Time.now.year, :activity => @activity.attributes
     assert_response 403
-  end
-  
-  test "admin can create activity" do
-    sign_in @admin_user
-    assert @activity.valid?, "activity is not valid"
-    assert_difference('Activity.count') do
-      post :create, :year => Time.now.year, :activity => @activity.attributes
-    end
-    assert_redirected_to activity_path(assigns(:activity))
   end
 
   test "anyone can show activity" do
