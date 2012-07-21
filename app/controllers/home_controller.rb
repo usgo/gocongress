5f322aac780da776cf711598bb5ba2ed250deef3
Jared Beck <jared@jaredbeck.com>
2012-07-20 22:35:07 -0400

    refactor into scopes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,7 @@ class HomeController < ApplicationController
   def index
     @bodyClassList = "homepage"
     @slides = SlideSet.new(@year.year).slides_as_arrays
-
-    # Get announcements for the homepage
-    contents_where_clause = "year = ? and show_on_homepage = ? and (expires_at is null or expires_at > ?)"
-    @contents = Content.where(contents_where_clause, @year.year, true, Time.now).order("created_at desc")
+    @contents = Content.yr(@year).homepage.unexpired.newest_first
   end
 
   def access_denied
--- app/models/content.rb
+++ app/models/content.rb
@@ -9,4 +9,14 @@ class Content < ActiveRecord::Base
   validates_length_of :subject, :maximum => 100
   validates_inclusion_of :show_on_homepage, :in => [true, false]
   validates_datetime :expires_at, :allow_blank => true
+
+  # Scopes
+  # -----
+
+  scope :homepage, where(show_on_homepage: true)
+  scope :newest_first, order("created_at desc")
+  scope :unexpired, lambda {
+    where("expires_at is null or expires_at > ?", Time.zone.now )
+  }
+
 end
