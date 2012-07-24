094b8afddc79bcc49323a6465b0bccfe199969f8
Jared Beck <jared@jaredbeck.com>
2012-07-24 01:10:21 -0400

    explicit default for mass_assignment_sanitizer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,10 @@ Gocongress::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Mass assignment protection for Active Record models
+  # See ActiveModel::MassAssignmentSecurity::ClassMethods
+  config.active_record.mass_assignment_sanitizer = :logger
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
