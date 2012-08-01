c756d176b670af268ca52d5918efb79acbda9b25
Jared Beck <jared@jaredbeck.com>
2012-08-01 19:01:54 -0400

    spec nesting

--- spec/models/attendee_plan_spec.rb
+++ spec/models/attendee_plan_spec.rb
@@ -1,12 +1,14 @@
 require "spec_helper"
 
 describe AttendeePlan do
+  describe "#valid?" do
 
-  it "is invalid when quantity exceeds plan max_quantity" do
-    a = FactoryGirl.create :attendee
-    p = FactoryGirl.create :plan, :max_quantity => 1
-    ap = AttendeePlan.new :plan_id => p.id, :quantity => p.max_quantity + 1, :attendee_id => a.id
-    ap.should_not be_valid
+
+    it "is invalid when quantity exceeds plan max_quantity" do
+      a = FactoryGirl.create :attendee
+      p = FactoryGirl.create :plan, :max_quantity => 1
+      ap = AttendeePlan.new :plan_id => p.id, :quantity => p.max_quantity + 1, :attendee_id => a.id
+      ap.should_not be_valid
+    end
   end
-
 end
