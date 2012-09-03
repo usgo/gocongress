1b658226537c0d3a14e339b9baaa8ee6bace498a
Jared Beck <jared@jaredbeck.com>
2012-09-03 00:42:02 -0400

    remove unnecessary context

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -3,32 +3,27 @@ require "spec_helper"
 describe Plan do
   it_behaves_like "a yearly model"
 
-  context "when it has an inventory of three" do
-    subject { FactoryGirl.create :plan, inventory: 3 }
+  context "when two attendees have selected it" do
+    let(:plan) { FactoryGirl.create :plan }
+    before do
+      1.upto(2) { plan.attendees << FactoryGirl.create(:attendee) }
+    end
 
-    context "when two attendees have selected it" do
-      before do
-        1.upto(2) { subject.attendees << FactoryGirl.create(:attendee) }
+    describe "#valid?" do
+      it "returns false when inventory is less than attendee count" do
+        plan.inventory = 1
+        plan.valid?.should be_false
       end
 
-      describe "#valid?" do
-        it "returns false when the inventory is reduced below the attendee count" do
-          subject.inventory = 1
-          subject.valid?.should be_false
-        end
+      it "returns true when inventory is equal to attendee count" do
+        plan.inventory = 2
+        plan.valid?.should be_true
+      end
 
-        it "returns true when the inventory is decreased to equal the attendee count" do
-          subject.inventory = 2
-          subject.valid?.should be_true
-        end
-
-        it "returns true when the inventory is increased" do
-          subject.inventory = 10
-          subject.valid?.should be_true
-        end
-      end # describe
-
-    end # context
-  end # context
-
-end # describe
+      it "returns true when inventory exceeds attendee count" do
+        plan.inventory = 10
+        plan.valid?.should be_true
+      end
+    end
+  end
+end
