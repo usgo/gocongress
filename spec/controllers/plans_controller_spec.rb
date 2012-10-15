e44ce719ebfcd3fea4f5f8c88076c8d94cd3f7eb
Jared Beck <jared@jaredbeck.com>
2012-10-14 20:44:16 -0400

    prefer single quotes

--- spec/controllers/plans_controller_spec.rb
+++ spec/controllers/plans_controller_spec.rb
@@ -1,15 +1,15 @@
 require "spec_helper"
 
 describe PlansController do
-  describe "DELETE destroy" do
-    it "fails when attendees have selected the plan" do
+  describe '#destroy' do
+    it 'fails when attendees have selected the plan' do
       plan = FactoryGirl.create :plan
       plan.attendees << FactoryGirl.create(:attendee)
       sign_in FactoryGirl.create :admin
       expect {
         delete :destroy, year: plan.year, id: plan.id
       }.to_not change{ Plan.count }
-      flash[:alert].should == "Cannot delete plan because attendees have already selected it"
+      flash[:alert].should == 'Cannot delete plan because attendees have already selected it'
       response.should redirect_to(plan_path(plan))
     end
   end
