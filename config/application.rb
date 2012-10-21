cd1e42532672c8a51395e960b777ddb0e47cb171
Jared Beck <jared@jaredbeck.com>
2012-10-21 17:20:49 -0400

    try removing smtp_settings[:domain] which should be optional

--- config/application.rb
+++ config/application.rb
@@ -34,8 +34,7 @@ module Gocongress
     config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
-      :authentication => :plain,
-      :domain => ENV['GMAIL_SMTP_USER'],
+      :authentication => 'plain',
       :user_name => ENV['GMAIL_SMTP_USER'],
       :password => ENV['GMAIL_SMTP_PASSWORD'],
       :enable_starttls_auto => true
