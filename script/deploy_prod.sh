405c29d157f7c3c9ad03a03d79643018ac35da5a
Jared Beck <jared@jaredbeck.com>
2012-11-03 19:24:01 -0400

    enter maint. mode *after* push

--- script/deploy_prod.sh
+++ script/deploy_prod.sh
@@ -1,4 +1,4 @@
-bundle exec heroku maintenance:on --app gocongress
 git push prod master
+bundle exec heroku maintenance:on --app gocongress
 bundle exec heroku run rake db:migrate --app gocongress
 bundle exec heroku maintenance:off --app gocongress
--- script/deploy_stage.sh
+++ script/deploy_stage.sh
@@ -1,4 +1,4 @@
-bundle exec heroku maintenance:on --app gocongress-dev
 git push stage master
+bundle exec heroku maintenance:on --app gocongress-dev
 bundle exec heroku run rake db:migrate --app gocongress-dev
 bundle exec heroku maintenance:off --app gocongress-dev
