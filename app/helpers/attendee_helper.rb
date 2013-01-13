b085afb98a7c80ba8382cfbf64df583da5e5733c
Jared Beck <jared@jaredbeck.com>
2013-01-13 18:06:20 -0500

    refactor from view to helpers

--- app/helpers/attendee_helper.rb
+++ app/helpers/attendee_helper.rb
@@ -4,4 +4,45 @@ module AttendeeHelper
     "#{a.address_1} #{a.address_2} #{a.city}, #{a.state} #{a.zip} #{a.country}"
   end
 
+  def plan_selection_inputs plan
+    selection = plan_selection(plan)
+    if plan.max_quantity == 1
+      plan_cbx(plan, selection)
+    else
+      plan_qty_field(plan, selection)
+    end
+  end
+
+  def plan_cbx plan, selection
+    checked = selection.qty == 1
+    title = "Select this " + mnh
+    check_box_tag qty_field_name(plan), 1, checked, :title => title
+  end
+
+  def plan_qty_field plan, selection
+    number_field_tag(
+      qty_field_name(plan),
+      selection.qty,
+      :size => 2,
+      :min => 0,
+      :max => plan.max_quantity,
+      :title => "Enter a quantity"
+    )
+  end
+
+  private
+
+  def mnh
+    Plan.model_name.human.downcase
+  end
+
+  def plan_selection plan
+    @plan_selections.select { |ps| ps.plan.id == plan.id }.first ||
+      Registration::PlanSelection.new(plan, 0)
+  end
+
+  def qty_field_name plan
+    "plan_#{plan.id}_qty"
+  end
+
 end
