9c8107363857a368def81f2f2ff66b916b75fb52
Jared Beck <jared@jaredbeck.com>
2013-01-27 01:14:39 -0500

    as a user it should deny access

--- spec/controllers/years_controller_spec.rb
+++ spec/controllers/years_controller_spec.rb
@@ -1,14 +1,30 @@
 require "spec_helper"
 
 describe YearsController do
+  let(:year) { Year.find_by_year(2013) }
+
+  context 'as a user' do
+    before do
+      sign_in create :user
+    end
+
+    it 'denies edit' do
+      get :edit, year: year.year
+      should_deny_access(response)
+    end
+
+    it 'denies update' do
+      put :update, year: year.year
+      should_deny_access(response)
+    end
+  end
+
   context 'as an admin' do
     before do
       sign_in create :admin
     end
 
     describe '#update' do
-      let(:year) { Year.find_by_year(2013) }
-
       it 'can update the city' do
         new_city = 'Terminator'
         expect {
@@ -23,5 +39,10 @@ describe YearsController do
         }.to raise_exception(ActiveModel::MassAssignmentSecurity::Error)
       end
     end
+  end
+
+  def should_deny_access response
+    response.should be_forbidden
+    response.should render_template :access_denied
   end
 end
