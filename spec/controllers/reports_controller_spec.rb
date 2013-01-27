a88a54c60e81c3e5156405f53ce9ffd64860c1f2
Jared Beck <jared@jaredbeck.com>
2013-01-27 17:22:14 -0500

    tests to specs

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -1,6 +1,24 @@
 require "spec_helper"
 
 describe ReportsController do
+  let(:admin) { create :admin }
+  render_views
+
+  it 'admin can get all reports' do
+    sign_in admin
+
+    # The following reports take no paramters, and only respond to html
+    %w[index invoices emails activities].each do |r|
+      get r, :year => admin.year
+      response.should be_success
+    end
+
+    # These reports take a min and max parameter
+    %w[atn_badges_all atn_reg_sheets user_invoices].each do |r|
+      get r, :year => admin.year, :min => 'a', :max => 'z'
+      response.should be_success
+    end
+  end
 
   describe "#user_invoices" do
     it "filters users alphabeticaly by name" do
