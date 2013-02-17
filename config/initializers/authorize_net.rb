19cfe2382382136c798d57d7954cf23d1ca2aae2
Jared Beck <jared@jaredbeck.com>
2013-02-16 20:10:16 -0500

    warn if api id undefined

--- config/initializers/authorize_net.rb
+++ config/initializers/authorize_net.rb
@@ -1,7 +1,7 @@
-# AUTHORIZE_NET_CONFIG = yml['default']
-# AUTHORIZE_NET_CONFIG.merge!(yml[RAILS_ENV]) unless yml[RAILS_ENV].nil?
-
 # https://devcenter.heroku.com/articles/config-vars
+if ENV['AUTHNET_API_LOGIN_ID'].nil?
+  Rails.logger.warn('AUTHNET_API_LOGIN_ID undefined')
+end
 AUTHORIZE_NET_CONFIG = {
   api_login_id: ENV['AUTHNET_API_LOGIN_ID'],
   api_transaction_key: ENV['AUTHNET_TRANSACTION_KEY'],
