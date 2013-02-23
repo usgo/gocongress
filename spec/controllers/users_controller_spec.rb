9e51713412dd317d61cb003a4df26b4d3b74842d
Jared Beck <jared@jaredbeck.com>
2013-02-22 23:26:39 -0500

    organize examples by role

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,26 +8,71 @@ shared_examples "successful get" do |action|
 end
 
 describe UsersController do
+  let(:user) { create :user }
+  let(:wrong_year) { user.year - 1 }
   let(:year) { Time.zone.now.year }
 
-  context "as a guest" do
+  context "as a visitor" do
     describe '#index' do
       it 'is forbidden' do
         get :index, :year => year
         response.should be_forbidden
       end
     end
+
+    describe '#show' do
+      it "is forbidden" do
+        get :show, :id => user.id, :year => user.year
+        response.should be_forbidden
+      end
+    end
   end
 
   context "as a user" do
-    before do
-      sign_in create :user
-    end
-
     describe '#index' do
       it 'is forbidden' do
+        sign_in user
         get :index, :year => year
         response.should be_forbidden
+      end
+    end
+
+    describe '#show' do
+      render_views
+
+      it "the same user succeeds" do
+        sign_in user
+        get :show, :id => user.id, :year => user.year
+        response.should be_successful
+      end
+
+      it "the same user in the wrong year raises RecordNotFound" do
+        sign_in user
+        expect { get :show, :id => user.id, :year => wrong_year
+          }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+
+      it "a different user from the same year is forbidden" do
+        sign_in create :user, year: user.year
+        get :show, :id => user.id, :year => user.year
+        response.should be_forbidden
+      end
+    end
+
+    describe '#update' do
+      it 'user cannot promote themselves' do
+        sign_in user
+        attrs = accessible_attributes_for(user).merge(role: 'A')
+        expect { put :update, :id => user.id, :user => attrs, :year => user.year
+          }.to_not change { user.reload.role }
+      end
+
+      it 'user can update own email address' do
+        sign_in user
+        new_email = 'derp' + user.email
+        attrs = accessible_attributes_for(user).merge(email: new_email)
+        expect { put :update, :id => user.id, :user => attrs, :year => user.year
+          }.to change { user.reload.email }
       end
     end
   end
@@ -43,6 +88,19 @@ describe UsersController do
         response.should be_success
       end
     end
+
+    describe '#show' do
+      it "succeeds" do
+        sign_in create :staff, year: user.year
+        get :show, :id => user.id, :year => user.year
+        response.should be_successful
+      end
+      it "from wrong year raises RecordNotFound" do
+        sign_in create :staff, year: wrong_year
+        expect { get :show, :id => user.id, :year => wrong_year
+          }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
   end
 
   context "as an admin" do
@@ -56,89 +114,26 @@ describe UsersController do
         response.should be_success
       end
     end
-  end
 
-  describe "#show" do
-    let(:user) { create :user, year: 2012 }
-    let(:wrong_year) { user.year - 1 }
-
-    def show id, year
-      get :show, {id: id, year: year}
-      response
-    end
-
-    context "as visitor" do
-      it "is forbidden" do
-        show(user.id, user.year).should be_forbidden
-      end
-    end
-
-    context "as user" do
-      render_views # rendering in a single context is sufficient
-      it "the same user succeeds" do
-        sign_in user
-        show(user.id, user.year).should be_successful
-      end
-      it "the same user in the wrong year raises RecordNotFound" do
-        sign_in user
-        expect { show(user.id, wrong_year)
-          }.to raise_error(ActiveRecord::RecordNotFound)
-      end
-      it "a different user from the same year is forbidden" do
-        sign_in create :user, year: user.year
-        show(user.id, user.year).should be_forbidden
-      end
-    end
-
-    context "as staff" do
-      it "succeeds" do
-        sign_in create :staff, year: user.year
-        show(user.id, user.year).should be_successful
-      end
-      it "from wrong year raises RecordNotFound" do
-        sign_in create :staff, year: wrong_year
-        expect { show(user.id, wrong_year)
-          }.to raise_error(ActiveRecord::RecordNotFound)
-      end
-    end
-
-    context "as admin" do
+    describe '#show' do
       it "succeeds" do
         sign_in create :admin, year: user.year
-        show(user.id, user.year).should be_successful
+        get :show, :id => user.id, :year => user.year
+        response.should be_successful
       end
       it "from wrong year raises RecordNotFound" do
         sign_in create :admin, year: wrong_year
-        expect { show(user.id, wrong_year)
+        expect { get :show, :id => user.id, :year => wrong_year
           }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
-  end
 
-  describe '#update' do
-    let(:user) { create :user }
-    let(:admin) { create :admin }
-
-    it 'user cannot promote themselves' do
-      sign_in user
-      attrs = accessible_attributes_for(user).merge(role: 'A')
-      expect { put :update, :id => user.id, :user => attrs, :year => user.year
-        }.to_not change { user.reload.role }
-    end
-
-    it 'user can update own email address' do
-      sign_in user
-      new_email = 'derp' + user.email
-      attrs = accessible_attributes_for(user).merge(email: new_email)
-      expect { put :update, :id => user.id, :user => attrs, :year => user.year
-        }.to change { user.reload.email }
-    end
-
-    it 'admin can update user' do
-      sign_in admin
-      attrs = accessible_attributes_for user
-      put :update, :id => user.id, :user => attrs, :year => user.year
-      response.should redirect_to user_path user
+    describe '#update' do
+      it 'can update any user' do
+        attrs = accessible_attributes_for user
+        put :update, :id => user.id, :user => attrs, :year => user.year
+        response.should redirect_to user_path user
+      end
     end
   end
 
