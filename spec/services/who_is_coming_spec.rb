f85d2197c9330dc2d93c5c903f4868ae352d1f8a
Jared Beck <jared@jaredbeck.com>
2013-03-26 23:30:17 -0400

    test that giant query

--- spec/services/who_is_coming_spec.rb
+++ spec/services/who_is_coming_spec.rb
@@ -2,7 +2,20 @@ require_relative '../../app/services/who_is_coming'
 
 describe WhoIsComing do
   describe '#attendees' do
-    it 'returns only attendees whose users have paid the required deposit'
-    it 'returns only attendees who have selected at least one plan'
+    it 'returns attendees that paid deposit and have at least one plan' do
+      p = create :plan
+
+      u1 = create :user
+      u1a = create :attendee, user: u1, given_name: 'u1a', plans: [p]
+      u1b = create :attendee, user: u1, given_name: 'u1b'
+      create :tr_sale, amount: 20000, user: u1
+
+      u2 = create :user
+      u2a = create :attendee, user: u2, given_name: 'u2a', plans: [p]
+      u2b = create :attendee, user: u2, given_name: 'u2b'
+      create :tr_sale, amount: 19999, user: u2
+
+      WhoIsComing.attendees(u1.year).map(&:given_name).should == ['u1a']
+    end
   end
 end
