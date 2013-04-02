1dc4757b3978b3f094217ac030a5d42567f70ca1
Jared Beck <jared@jaredbeck.com>
2013-04-02 01:34:08 -0400

    AUTHNET_MERCHANT_HASH

--- config/initializers/authorize_net.rb
+++ config/initializers/authorize_net.rb
@@ -5,5 +5,5 @@ end
 AUTHORIZE_NET_CONFIG = {
   api_login_id: ENV['AUTHNET_API_LOGIN_ID'],
   api_transaction_key: ENV['AUTHNET_TRANSACTION_KEY'],
-  merchant_hash_value: ENV['AUTHNET_API_LOGIN_ID']
+  merchant_hash_value: ENV['AUTHNET_MERCHANT_HASH']
 }.stringify_keys.freeze
