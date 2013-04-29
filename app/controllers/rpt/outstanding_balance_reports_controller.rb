25918dcf5ce115ef3fc5c927e590fc9673269fd1
Jared Beck <jared@jaredbeck.com>
2013-04-28 20:52:49 -0400

    fix error when user had no primary_attendee
    primary_attendee is deprecated

--- app/controllers/rpt/outstanding_balance_reports_controller.rb
+++ app/controllers/rpt/outstanding_balance_reports_controller.rb
@@ -5,7 +5,6 @@ def show
   # When generating invoices for multiple users, the `includes`
   # can really speed things up.
   @users = User.yr(@year).includes([
-    :primary_attendee,
     {
       :attendees => [
         {:attendee_activities => :activity},
@@ -18,11 +17,6 @@ def show
   # users who owe us money, but it is also useful for the registrar
   # to see people who deserve refunds.
   @users.delete_if { |u| (-0.01..0.01).cover?(u.balance) }
-
-  # Sort by family_name.  Normally we would do this in the query,
-  # but joining on primary_attendee would conflict with our eager
-  # loading configuration.
-  @users.sort!{|a,b| a.primary_attendee.family_name <=> b.primary_attendee.family_name}
 
   @email_list = @users.map(&:email).join(', ')
 end
--- spec/controllers/rpt/outstanding_balance_reports_controller_spec.rb
+++ spec/controllers/rpt/outstanding_balance_reports_controller_spec.rb
@@ -9,17 +9,17 @@ describe Rpt::OutstandingBalanceReportsController do
       sign_in create :admin
 
       # One user paid too little
-      a1 = create :attendee, is_primary: true
+      a1 = create :attendee
       a1.plans << create(:plan)
       paid_too_little = a1.user
 
       # One user paid too much
-      a2 = create :attendee, is_primary: true
+      a2 = create :attendee
       paid_too_much = a2.user
       paid_too_much.transactions << create(:tr_sale)
 
       # The other paid juuuuuust right
-      a3 = create :attendee, is_primary: true
+      a3 = create :attendee
       paid_exactly = a3.user
 
       get :show, :year => Time.now.year
