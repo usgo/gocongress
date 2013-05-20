095598ac44f7b924a81310ea0fb184b97d35e727
Jared Beck <jared@jaredbeck.com>
2013-05-19 20:55:28 -0400

    remove deprecated `primary_attendee`

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -13,10 +13,8 @@ module UsersHelper
   def user_account_title user, page
     if signed_in? && current_user == user
       "My #{page}"
-    elsif user.primary_attendee.present?
-      "#{user.full_name_possessive} #{page}"
     else
-      page
+      "#{page}: #{user.email}"
     end
   end
 
