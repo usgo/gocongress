a549a78721458052a49443ae9681e2d24e787342
Jared Beck <jared@jaredbeck.com>
2013-08-06 13:46:56 -0700

    confirm that `parse_params` handles disabled plans

--- spec/models/registration/plan_selection_spec.rb
+++ spec/models/registration/plan_selection_spec.rb
@@ -8,25 +8,28 @@ describe Registration::PlanSelection do
     subject { Registration::PlanSelection }
     let(:another_plan) { create(:plan) }
     let(:daily_plan) { create(:plan, daily: true) }
-    let(:plans) { [plan, another_plan, daily_plan] }
+    let(:disabled_plan) { create(:plan, disabled: true) }
+    let(:plans) { [plan, another_plan, daily_plan, disabled_plan] }
 
     it 'builds an array of selections from the params' do
       plan_parms = {
         plan.id.to_s => { 'qty' => 2 },
         daily_plan.id.to_s => { 'qty' => 1, 'dates' => dates },
-        '12345' => { 'qty' => 42 }
+        '12345' => { 'qty' => 42 },
+        disabled_plan.id.to_s => { 'qty' => '13' }
       }
       result = subject.parse_params(plan_parms, plans)
       result.count.should == plans.count
       result.map(&:plan).should =~ plans
-      result.map{|s| s.qty}.should =~ [0,1,2]
+      result.map{|s| s.qty}.should =~ [0,1,2,13]
       daily_plan_sln = result.select{|s| s.plan.id == daily_plan.id}.first
       parsed_dates = dates.map { |d| Date.parse(d) }
       daily_plan_sln.dates.should =~ parsed_dates
       result.should =~ [
         ps(plan, 2),
         ps(daily_plan, 1, parsed_dates),
-        ps(another_plan, 0)
+        ps(another_plan, 0),
+        ps(disabled_plan, 13)
       ]
     end
   end
