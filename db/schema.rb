1318d51f7a8e0bb4d26cb9054ffb3f3fc83bbe5b
Rexford Cristal <rcristal3@netscape.net>
2013-09-23 17:30:27 -0400

    Set up a 2014 site

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130627040731) do
+ActiveRecord::Schema.define(:version => 20130923073837) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
