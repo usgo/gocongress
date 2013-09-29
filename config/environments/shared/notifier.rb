9b7d2a1057d926d85dbb2be43c17610d7bfad748
Jared Beck <jared@jaredbeck.com>
2013-09-28 22:28:37 -0400

    upgrades to exception_notification 4

--- config/environments/shared/notifier.rb
+++ config/environments/shared/notifier.rb
@@ -1,7 +1,10 @@
 module GocongressNotifier
   def self.use_exception_notifier_middleware config
-    config.middleware.use ExceptionNotifier,
-      sender_address: 'usgcwebsite@gmail.com',
-      exception_recipients: 'jared@jaredbeck.com'
+    config.middleware.use ExceptionNotification::Rack,
+      :email => {
+        :email_prefix => "[USGC] ",
+        :sender_address => 'usgcwebsite@gmail.com',
+        :exception_recipients => 'jared@jaredbeck.com'
+      }
   end
 end
