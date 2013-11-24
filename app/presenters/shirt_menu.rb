8f94774016202d9ecce1ce559ded5cce457ae9f2
Jared Beck <jared@jaredbeck.com>
2013-11-24 02:58:26 -0500

    only show shirts from current year

--- app/presenters/shirt_menu.rb
+++ app/presenters/shirt_menu.rb
@@ -1,15 +1,20 @@
 class ShirtMenu
   extend ActionView::Helpers::FormOptionsHelper
 
-  def self.to_html f, selected_shirt_id
-    f.select :shirt_id, options(selected_shirt_id), :include_blank => ''
+  def self.to_html f, year, selected_shirt_id
+    s = shirts(year, selected_shirt_id)
+    if s.size == 0
+      "TBD"
+    else
+      f.select :shirt_id, options(s, selected_shirt_id), :include_blank => ''
+    end
   end
 
   private
 
-  def self.options selected_shirt_id
+  def self.options shirt_set, selected_shirt_id
     options_from_collection_for_select(
-      shirts(selected_shirt_id), :id, :name, selected = selected_shirt_id)
+      shirt_set, :id, :name, selected = selected_shirt_id)
   end
 
   def self.selected_shirt_array id
@@ -20,7 +25,7 @@ class ShirtMenu
     end
   end
 
-  def self.shirts selected_shirt_id
-    Set.new(Shirt.enabled).merge(selected_shirt_array(selected_shirt_id)).to_a
+  def self.shirts year, selected_shirt_id
+    Set.new(Shirt.yr(year).enabled).merge(selected_shirt_array(selected_shirt_id)).to_a
   end
 end
