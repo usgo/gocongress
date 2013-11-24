35e4a60647ddc47bf7af70274c4328c48911c7c7
Jared Beck <jared@jaredbeck.com>
2013-11-24 16:08:59 -0500

    tweaks body margin when no sponsors added yet, adds aga to 2014

--- app/models/year.rb
+++ app/models/year.rb
@@ -83,7 +83,7 @@ private
       2011 => [:aga, :kaba, :kgs],
       2012 => [:aga, :agf, :confucius, :jpga, :kaba, :pandanet, :slate_and_shell, :tygem],
       2013 => [],
-      2014 => []
+      2014 => [:aga]
     }
   end
 
