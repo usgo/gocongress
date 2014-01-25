85533f2c2e65bc7e460f6fa0d1652ffc31d33475
Jared Beck <jared@jaredbeck.com>
2014-01-25 15:48:06 -0500

    Fixes mismatched years in tests

--- spec/controllers/years_controller_spec.rb
+++ spec/controllers/years_controller_spec.rb
@@ -1,11 +1,12 @@
 require "spec_helper"
 
 describe YearsController do
-  let(:year) { Year.find_by_year(2013) }
+  let(:y) { 2013 }
+  let(:year) { Year.find_by_year(y) }
 
   context 'as a user' do
     before do
-      sign_in create :user
+      sign_in create :user, year: y
     end
 
     it 'denies edit' do
@@ -20,23 +21,41 @@ describe YearsController do
   end
 
   context 'as an admin' do
-    before do
-      sign_in create :admin
-    end
+    let(:new_city) { 'Terminator' }
 
-    describe '#update' do
-      it 'can update the city' do
-        new_city = 'Terminator'
-        expect {
-          put :update, {year: year.year, year_record: {city: new_city}}
-        }.to_not raise_exception
-        year.reload.city.should == new_city
+    context 'from same year' do
+      before do
+        sign_in create :admin, year: y
       end
 
-      it 'cannot update the year attribute' do
-        expect {
-          put :update, {year: year.year, year_record: {year: 2312}}
-        }.to raise_exception(ActiveModel::MassAssignmentSecurity::Error)
+      describe '#update' do
+        it 'can update the city' do
+          expect {
+            put :update, {year: year.year, year_record: {city: new_city}}
+          }.to_not raise_exception
+          year.reload.city.should == new_city
+        end
+
+        it 'cannot update the year attribute' do
+          expect {
+            put :update, {year: year.year, year_record: {year: 2312}}
+          }.to raise_exception(ActiveModel::MassAssignmentSecurity::Error)
+        end
+      end
+    end
+
+    context 'from different year' do
+      before do
+        sign_in create :admin, year: y + 1
+      end
+
+      describe '#update' do
+        it 'is forbidden' do
+          expect {
+            put :update, {year: year.year, year_record: {city: new_city}}
+          }.to_not change { year.reload.city }
+          expect(response).to be_forbidden
+        end
       end
     end
   end
