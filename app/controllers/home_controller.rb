822bc519cab7c27ee6562b552108f999384b7c0a
Rexford Cristal <rcristal3@netscape.net>
2014-05-09 00:04:57 -0400

    Adds logo for 2014

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -25,7 +25,13 @@ protected
   private
 
   def logo_file year
-    year.to_i == 2013 ? '2013.jpg' : "#{@year.year}.png"
+    if year.to_i == 2014
+      '2014-logo.png'
+    elsif year.to_i == 2013
+      '2013.jpg'
+    else
+      "#{@year.year}.png"
+    end
   end
 
   private
