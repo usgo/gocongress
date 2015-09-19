9327f6534cc20cd97b26d6e184121f2d1596160a
Rexford Cristal <rcristal3@netscape.net>
2015-09-19 18:22:16 -0400

    Adds Alternate Name to daily plan report

--- app/exporters/daily_plan_export.sql
+++ app/exporters/daily_plan_export.sql
@@ -3,6 +3,7 @@ select
   a.id as attendee_id,
   a.family_name,
   a.given_name,
+  a.alternate_name,
   p.name as plan_name,
   min(apd._date) as first_date,
   max(apd._date) as last_date
--- spec/exporters/daily_plan_csv_exporter_spec.rb
+++ spec/exporters/daily_plan_csv_exporter_spec.rb
@@ -8,7 +8,7 @@ describe DailyPlanCsvExporter do
       p1 = create :plan, daily: true, name: 'Plan 1'
       p2 = create :plan, daily: true, disabled: false, name: 'Plan 2'
       p3 = create :plan, daily: false
-      a1 = create :attendee
+      a1 = create :attendee, alternate_name: 'Alternate Name'
       a1ap1 = create :attendee_plan, attendee: a1, plan: p1
       a1ap2 = create :attendee_plan, attendee: a1, plan: p2
       a1ap1d1 = create :attendee_plan_date, attendee_plan: a1ap1, _date: csd
@@ -24,15 +24,17 @@ describe DailyPlanCsvExporter do
       expect(ary[0][1]).to eq('attendee_id')
       expect(ary[0][2]).to eq('Family Name')
       expect(ary[0][3]).to eq('Given Name')
-      expect(ary[0][4]).to eq('Plan 1') # should be alphabetical
-      expect(ary[0][5]).to eq('Plan 2')
+      expect(ary[0][4]).to eq('Alternate Name')
+      expect(ary[0][5]).to eq('Plan 1') # should be alphabetical
+      expect(ary[0][6]).to eq('Plan 2')
 
       expect(ary[1][0]).to eq(a1.user_id.to_s)
       expect(ary[1][1]).to eq(a1.id.to_s)
       expect(ary[1][2]).to eq(a1.family_name)
       expect(ary[1][3]).to eq(a1.given_name)
-      expect(ary[1][4]).to eq(format_date_range(a1ap1d1._date..a1ap1d2._date))
-      expect(ary[1][5]).to eq(format_date_range(a1ap2d1._date..a1ap2d1._date))
+      expect(ary[1][4]).to eq(a1.alternate_name)
+      expect(ary[1][5]).to eq(format_date_range(a1ap1d1._date..a1ap1d2._date))
+      expect(ary[1][6]).to eq(format_date_range(a1ap2d1._date..a1ap2d1._date))
     end
 
     def format_date d
