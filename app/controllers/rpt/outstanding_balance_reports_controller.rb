f5be69030e9476baadc0012b1934e861ab08b527
Rexford Cristal <rcristal3@netscape.net>
2016-08-06 18:03:01 -0400

    Paginate outstanding balance report

--- app/controllers/rpt/outstanding_balance_reports_controller.rb
+++ app/controllers/rpt/outstanding_balance_reports_controller.rb
@@ -1,24 +1,25 @@
 class Rpt::OutstandingBalanceReportsController < Rpt::AbstractReportController
+  # Pagination
+  PER_PAGE = 100
 
-def show
+  def show
+    # When generating invoices for multiple users, the `includes`
+    # can really speed things up.
+    @users = User.yr(@year).includes([
+      {
+        :attendees => [
+          {:attendee_activities => :activity},
+          {:attendee_plans => :plan}
+        ]
+      }
+    ]).page(params[:page]).per(PER_PAGE)
+    @users_array = @users.to_a
 
-  # When generating invoices for multiple users, the `includes`
-  # can really speed things up.
-  @users = User.yr(@year).includes([
-    {
-      :attendees => [
-        {:attendee_activities => :activity},
-        {:attendee_plans => :plan}
-      ]
-    }
-  ]).to_a
+    # Keep users with non-zero balances.  Obviously, we want to see
+    # users who owe us money, but it is also useful for the registrar
+    # to see people who deserve refunds.
+    @users_array.delete_if { |u| (-0.01..0.01).cover?(u.balance) }
 
-  # Keep users with non-zero balances.  Obviously, we want to see
-  # users who owe us money, but it is also useful for the registrar
-  # to see people who deserve refunds.
-  @users.delete_if { |u| (-0.01..0.01).cover?(u.balance) }
-
-  @email_list = @users.map(&:email).join(', ')
-end
-
+    @email_list = @users_array.map(&:email).join(', ')
+  end
 end
