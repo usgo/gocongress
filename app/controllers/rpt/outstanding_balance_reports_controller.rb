76220f0fb8516e58823de1a545dd0603b6af1024
Rexford Cristal <rcristal3@netscape.net>
2016-08-10 01:26:42 -0400

    Removes pagination

--- app/controllers/rpt/outstanding_balance_reports_controller.rb
+++ app/controllers/rpt/outstanding_balance_reports_controller.rb
@@ -1,7 +1,4 @@
 class Rpt::OutstandingBalanceReportsController < Rpt::AbstractReportController
-  # Pagination
-  PER_PAGE = 100
-
   def show
     # When generating invoices for multiple users, the `includes`
     # can really speed things up.
@@ -12,14 +9,13 @@ class Rpt::OutstandingBalanceReportsController < Rpt::AbstractReportController
           {:attendee_plans => :plan}
         ]
       }
-    ]).page(params[:page]).per(PER_PAGE)
-    @users_array = @users.to_a
+    ]).to_a
 
     # Keep users with non-zero balances.  Obviously, we want to see
     # users who owe us money, but it is also useful for the registrar
     # to see people who deserve refunds.
-    @users_array.delete_if { |u| (-0.01..0.01).cover?(u.balance) }
+    @users.delete_if { |u| (-0.01..0.01).cover?(u.balance) }
 
-    @email_list = @users_array.map(&:email).join(', ')
+    @email_list = @users.map(&:email).join(', ')
   end
 end
