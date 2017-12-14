ad1b45f444c493be6aa12a32be88e68b07ec8bc4
Rexford Cristal <rcristal3@netscape.net>
2017-12-14 14:18:36 -0500

    Protect Contact model

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,7 +1,15 @@
 require "rails_helper"
 
 RSpec.describe ContactsController, :type => :controller do
-  it_behaves_like "an admin controller", :contact do
-    let(:updateable_attribute) { :family_name }
+  let(:contact) { create :contact }
+
+  describe "#update" do
+    it "succeeds" do
+      sign_in create :admin
+      expect {
+        patch :update, :year => contact.year, :id => contact.id,
+          :contact => {list_order: 1}
+      }.to change{ contact.reload.list_order }.to(1)
+    end
   end
 end
