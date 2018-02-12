3e5793a970b0e1f7271b266da7e4d933d07fdd3f
Rexford Cristal <rcristal3@netscape.net>
2018-02-11 22:52:29 -0500

    Protect Year model

--- app/models/year.rb
+++ app/models/year.rb
@@ -1,15 +1,7 @@
 class Year < ActiveRecord::Base
+  include ActiveModel::ForbiddenAttributesProtection
 
   REG_PHASES = %w[closed open complete]
-
-  # Mass Assignment Security
-  # ------------------------
-
-  attr_accessible :city, :date_range, :day_off_date,
-    :ordinal_number, :registration_phase, :reply_to_email,
-    :start_date, :state, :timezone, :twitter_url, :venue_name,
-    :venue_address, :venue_city, :venue_state, :venue_zip,
-    :venue_url, :venue_phone
 
   # Validations
   # -----------
