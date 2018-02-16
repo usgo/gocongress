82835073e0bc9180fd62ee1125c30998b1c7ca50
Rexford Cristal <rcristal3@netscape.net>
2018-02-15 21:20:12 -0500

    Protect Transaction model

--- spec/controllers/transactions_controller_spec.rb
+++ spec/controllers/transactions_controller_spec.rb
@@ -1,9 +1,11 @@
 require "rails_helper"
 
 RSpec.describe TransactionsController, :type => :controller do
+  let(:transaction_attributes) { { :trantype => "S", :amount => 1, :instrument => "C", :gwdate => Time.utc(Time.now.year, 8, 1), :gwtranid => 1 } }
+
   it_behaves_like "an admin controller", :transaction do
     let(:user) { create :user }
-    let(:extra_params_for_create) { {:user_email => user.email} }
+    let(:extra_params_for_create) { { :transaction => transaction_attributes, :user_email => user.email } }
     let(:updateable_attribute) { :comment }
   end
 
@@ -20,14 +22,14 @@ RSpec.describe TransactionsController, :type => :controller do
         post :create,
           :year => user.year,
           :user_email => bad_email,
-          :transaction => accessible_attributes_for(:tr_sale)
+          :transaction => transaction_attributes
       }.to_not change{ Transaction.count }
       expect(response).to render_template :new
     end
 
     it "will not update the transaction" do
       t = create :tr_sale
-      expect { put_update(t, bad_email) }.to_not change{ t.user.email }
+      expect { patch_update(t, bad_email) }.to_not change{ t.user.email }
       expect(response).to render_template :edit
     end
   end
@@ -42,17 +44,17 @@ RSpec.describe TransactionsController, :type => :controller do
 
       it "redirects to the correct year" do
         t = create :tr_sale, { user: user, year: 2011 }
-        put_update(t, t.user.email)
+        patch_update(t, t.user.email)
         expect(response).to redirect_to transaction_path(id: t.id, year: t.year)
       end
     end
   end
 
-  def put_update t, email
-    put :update,
+  def patch_update t, email
+    patch :update,
       :year => t.year,
       :user_email => email,
       :id => t.id,
-      :transaction => accessible_attributes_for(t)
+      :transaction => transaction_attributes
   end
 end
