459ffce5e631137d164d049b38061d52150ad835
Rexford Cristal <rcristal3@netscape.net>
2018-02-17 17:32:20 -0500

    Removes config.active_record.whitelist_attributes

--- config/application.rb
+++ config/application.rb
@@ -75,7 +75,5 @@ module Gocongress
     # -----------------
     config.action_controller.permit_all_parameters = false
     config.action_controller.action_on_unpermitted_parameters = :raise
-
-    config.active_record.whitelist_attributes = false
   end
 end
