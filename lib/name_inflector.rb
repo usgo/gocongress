349bfb683c3db3c17df1ce8064d7b00baaac721b
compostbrain <joelcahalan@gmail.com>
2018-04-23 10:22:26 -0400

    Fix name_inflector.rb to capitalize edge cases
    The name_inflector module was not properly dealing with some edge
    cases as reflected in some tests I added. Somehow the tests got added to
    master and not the refactor I did to pass the tests. This commit fixes
    that.

--- lib/name_inflector.rb
+++ lib/name_inflector.rb
@@ -5,43 +5,45 @@ module NameInflector
   MANDPREF = ["o'"]
 
   # Suffixes with mandatory capitalization
-  MANDSUF = ["jr.", "sr."]
+  MANDSUF = ["jr.", "sr.", "esq."]
 
   # Prefixes with optional succeeding capitals, eg.
   # Macintyre and MacIntyre are both valid
   OPTPREF = ["mac", "mc"]
 
   def self.capitalize(name)
-    mandpref_match = longest_matching_prefix MANDPREF, name
-    mandsuf_match = longest_matching_suffix MANDSUF, name
-    optpref_match = longest_matching_prefix OPTPREF, name
-
-    if mandpref_match.present? || mandsuf_match.present? || optpref_match.present?
-
-      if mandpref_match.present?
-        postprefix = remove_prefix name, mandpref_match
-        return mandpref_match.capitalize + postprefix.capitalize
-      end
-
-      if mandsuf_match.present?
-        presuffix = remove_suffix name, mandsuf_match
-        return presuffix.capitalize + mandsuf_match.capitalize
-      end
-
-      if optpref_match.present?
-        postprefix = remove_prefix name, optpref_match
-        return optpref_match.capitalize + postprefix
-      end
-
-    elsif name.include? '-'
-      return name.split('-').map{|n| n.capitalize}.join('-')
-
+    if name.include? "-"
+      name.split("-").map{ |n| capitalize_name(n) }.join("-")
     else
-      return name.capitalize
+      capitalize_name(name)
     end
   end
 
   private
+
+  def self.capitalize_name(name)
+    mandpref_match = longest_matching_prefix MANDPREF, name
+    mandsuf_match = longest_matching_suffix MANDSUF, name
+    optpref_match = longest_matching_prefix OPTPREF, name
+
+    if mandpref_match.present?
+      postprefix = remove_prefix name, mandpref_match
+      name_fix = mandpref_match.capitalize + postprefix.capitalize
+
+    elsif optpref_match.present?
+      postprefix = remove_prefix name, optpref_match
+      is_first_char_uppercase?(postprefix) ? name_fix = name : name_fix = name.capitalize
+
+    else
+      name_fix = name.capitalize
+    end
+    capitalize_suffix(name_fix, mandsuf_match) if mandsuf_match.present?
+    return name_fix
+  end
+
+  def self.capitalize_suffix(name, suffix)
+    name.sub!("#{suffix}", "#{suffix.capitalize}")
+  end
 
   def self.is_first_char_uppercase?(string)
     ("A".."Z").cover? string.slice(0)
@@ -55,10 +57,6 @@ module NameInflector
     return longest
   end
 
-  def self.remove_prefix(string, prefix)
-    string.slice(prefix.length, string.length - prefix.length)
-  end
-
   def self.longest_matching_suffix(suffixes, string)
     matches = suffixes.select{|s| string.downcase.end_with?(s)}
     return nil if matches.empty?
@@ -67,7 +65,7 @@ module NameInflector
     return longest
   end
 
-  def self.remove_suffix(string, suffix)
-    string.slice(0, string.length - suffix.length)
+  def self.remove_prefix(string, prefix)
+    string.slice(prefix.length, string.length - prefix.length)
   end
 end
