83ad43d8aff23bacabea85f33c4204a52ca375b7
compostbrain <joelcahalan@gmail.com>
2018-05-04 12:18:34 -0400

    Add validation tests for game_appointment model

--- spec/factories/game_appointments.rb
+++ spec/factories/game_appointments.rb
@@ -3,8 +3,9 @@ FactoryBot.define do
     opponent "Gu Li"
     location "Building 6 Room 24"
     time "4:30 PM"
+    time_zone "Eastern Standard"
     year Time.now.year
     attendee
-
+    tournament
   end
 end
