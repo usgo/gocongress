9e11f3bffffcef89b1c108465fe34368d51e5e12
compostbrain <joelcahalan@gmail.com>
2018-05-08 17:38:23 -0400

    Fixed validation for local phone

--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Round, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(build(:round)).to be_valid
+  end
+
+
 end
