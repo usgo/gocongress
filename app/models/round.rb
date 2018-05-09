f53e31352614607942a4a8bc19459e64197c3993
compostbrain <joelcahalan@gmail.com>
2018-05-09 14:07:03 -0400

    Added send_sms_reminders to RoundsController

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,7 +2,7 @@ class Round < ApplicationRecord
   include YearlyModel
 
   belongs_to :tournament
-
+  has_many :game_appointments, dependent: :destroy
   validates :number, presence: true
   validates :start_time, presence: true
 end
