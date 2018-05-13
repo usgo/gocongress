e5ed419ea4c5bd353c3058547992d7ee1c5cd060
Nate Eagle <nate@nateeagle.com>
2018-05-13 00:40:22 -0400

    Use import from XML to create game_appointments
    * Can now actually create game appointments! Import will need to have
      aga_ids of attendees you actually have in your DB. We might need to
      create a manual way of generating an import file for development.
    
    * Add a "delete all" button for rounds to let a user easily get rid of
      all game appointments (probably useful in general in case of a bad
      import, but definitely useful for development testing)
    
    * Add uniqueness validation to make sure that attendees can't play in
      more than one game per round, nor can one table be present twice

--- app/models/game_appointment.rb
+++ app/models/game_appointment.rb
@@ -5,19 +5,41 @@ class GameAppointment < ApplicationRecord
   belongs_to :attendee_two, class_name: "Attendee",  foreign_key: "attendee_two_id"
   belongs_to :round
 
-  validates :attendee_one, presence: true
-  validates :attendee_two, presence: true
+  validates :attendee_one, presence: true, uniqueness: { scope: :round,
+    message: "players can only play in one game per round" }
+  validates :attendee_two, presence: true, uniqueness: { scope: :round,
+    message: "players can only play in one game per round" }
+
   validate :compare_attendees
   validates :round, presence: true
-  validates :table, presence: true
+  validates :table, presence: true, uniqueness: { scope: :round, message: "a
+    table can only have one game per round"}
   validates :location, presence: true
   validates :time, presence: true
-  validates :time_zone, presence: true
+  # TODO: Remove time_zone
+  # validates :time_zone, presence: true
 
   def compare_attendees
     if self.attendee_one == self.attendee_two
       errors.add("Attendees", "can't be the same person")
     end
+  end
+
+  def self.assign_from_hash(round, appointment)
+    white = appointment['whitePlayer']
+    black = appointment['blackPlayer']
+    game_appointment = GameAppointment.new
+    game_appointment.attendee_one = Attendee.find_by_aga_id(white['agaId'])
+    game_appointment.attendee_two = Attendee.find_by_aga_id(black['agaId'])
+    game_appointment.round = round
+    game_appointment.table = appointment['tableNumber'].to_i
+    game_appointment.time = round.start_time
+    game_appointment.year = round.year
+
+    tournament = Tournament.find(round.tournament_id)
+    game_appointment.location = tournament.location
+
+    game_appointment
   end
 
   # after_create :reminder
