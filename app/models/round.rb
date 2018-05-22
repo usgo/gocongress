f24f295c62df071135e0bbbdbda2098a3df2803a
compostbrain <joelcahalan@gmail.com>
2018-05-22 16:39:05 -0400

    Deleted stray files

--- app/models/round.rb
+++
@@ -1,6 +0,0 @@
-class Round < ApplicationRecord
-  belongs_to :tournament
-
-  validates :number, presence: true
-  validates :start_time, presence: true
-end
--- spec/models/round_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Round, type: :model do
-  it "has a valid factory" do
-    expect(build(:round)).to be_valid
-  end
-
-
-end
