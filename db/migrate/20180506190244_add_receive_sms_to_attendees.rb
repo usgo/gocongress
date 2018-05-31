532093c3340eb8f418c72d73a343b3976adbbe87
compostbrain <joelcahalan@gmail.com>
2018-05-31 13:44:55 -0400

    Fix default selection for receive_sms radio button

--- db/migrate/20180506190244_add_receive_sms_to_attendees.rb
+++ db/migrate/20180506190244_add_receive_sms_to_attendees.rb
@@ -1,5 +1,5 @@
 class AddReceiveSmsToAttendees < ActiveRecord::Migration[5.0]
   def change
-    add_column :attendees, :receive_sms, :boolean, default: false
+    add_column :attendees, :receive_sms, :boolean, default: nil
   end
 end
