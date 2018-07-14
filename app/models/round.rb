9bab20013c86d1df5b11847ffd7f73a4440fd29a
compostbrain <joel.cahalan@gmail.com>
2018-07-14 07:03:21 -0400

    Add default scope for game appointments to order by table

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,7 +2,7 @@ class Round < ApplicationRecord
   include YearlyModel
 
   belongs_to :tournament
-  has_many :game_appointments, dependent: :destroy
+  has_many :game_appointments, -> { order(:table) }, dependent: :destroy
   validates :number, presence: true
   validates :number, uniqueness: { scope: :tournament_id, message: "A round with that number already exists for the tournament."}
   validates :start_time, presence: true
