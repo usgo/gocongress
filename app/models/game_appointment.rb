27955ac42003f2ad62ad9d773619346172c0d610
compostbrain <joel.cahalan@gmail.com>
2018-07-14 14:25:15 -0400

    Update import to save result and handicap attributes

--- app/models/game_appointment.rb
+++ app/models/game_appointment.rb
@@ -29,12 +29,14 @@ class GameAppointment < ApplicationRecord
     white = appointment['whitePlayer']
     black = appointment['blackPlayer']
     game_appointment = GameAppointment.new
-    game_appointment.attendee_one = Attendee.find_by_aga_id(white['agaId'])
-    game_appointment.attendee_two = Attendee.find_by_aga_id(black['agaId'])
+    game_appointment.white_player = Attendee.find_by_aga_id(white['agaId'])
+    game_appointment.black_player = Attendee.find_by_aga_id(black['agaId'])
     game_appointment.round = round
     game_appointment.table = appointment['tableNumber'].to_i
     game_appointment.time = round.start_time
     game_appointment.year = round.year
+    game_appointment.result = appointment["result"]
+    game_appointment.handicap = appointment["handicap"].to_i
 
     tournament = Tournament.find(round.tournament_id)
     game_appointment.location = tournament.location.empty? ? "N/A" : tournament.location
