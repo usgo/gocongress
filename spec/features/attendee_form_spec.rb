4f8c88a31648b20cac64ec3d1cc063588096a850
Rexford Cristal <rcristal3@netscape.net>
2019-03-01 01:33:39 -0500

    Shows balance to admins only

--- spec/features/attendee_form_spec.rb
+++ spec/features/attendee_form_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe 'registration form', :type => :feature do
     fill_in 'Password', with: password
     click_button 'Sign in'
     expect(page).to have_content('My Account')
-    expect(page).to have_selector('form')
     visit new_registration_path(year: user.year, user_id: user.id, type: 'adult')
     expect(page).to have_content('First Attendee')
     expect(page).to have_selector('form')
