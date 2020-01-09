148d90c9e5b06ebbfacc9ae6029fde10644396b9
Nate Eagle <nate.eagle@deciphernow.com>
2020-01-09 15:10:53 -0500

    Add dynamic publishable key for stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,13 @@
 # https://devcenter.heroku.com/articles/config-vars
+if ENV['STRIPE_PUBLISHABLE_KEY'].nil?
+  Rails.logger.warn('STRIPE_PUBLISHABLE_KEY undefined')
+end
+
 if ENV['STRIPE_SECRET_KEY'].nil?
   Rails.logger.warn('STRIPE_SECRET_KEY undefined')
 end
 
+::STRIPE_PUBLISHABLE_KEY = ENV['STRIPE_PUBLISHABLE_KEY']
 Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 StripeEvent.signing_secret = ENV['STRIPE_SIGNING_SECRET']
 
