1ff4f8a6739d7fa36021eeb9547415ee9dc08780
Nate Eagle <nate.eagle@deciphernow.com>
2020-01-13 11:32:12 -0500

    Fix galleria URL by prepending slash /

--- app/assets/javascripts/usgc.js
+++ app/assets/javascripts/usgc.js
@@ -1,23 +1,24 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
   // Initialize any dialogs present
-  $( ".dialog" ).dialog({
+  $(".dialog").dialog({
     autoOpen: false,
     modal: true,
     width: 450
   });
 
   /* Fire up the slideshow -Jared 11/30/10 */
-  $('#gallery-container').show();
-  if ($('#gallery').length == 1) {
-    Galleria.loadTheme('assets/galleria/themes/classic/galleria.classic.min.js');
-    $('#gallery').galleria({
-      autoplay: 5000
-      , image_crop: true
-      , show_info: true
-      , thumbnails: false
-      , transition: 'fade'
-      , transition_speed: 1000
+  $("#gallery-container").show();
+  if ($("#gallery").length == 1) {
+    Galleria.loadTheme(
+      "/assets/galleria/themes/classic/galleria.classic.min.js"
+    );
+    $("#gallery").galleria({
+      autoplay: 5000,
+      image_crop: true,
+      show_info: true,
+      thumbnails: false,
+      transition: "fade",
+      transition_speed: 1000
     });
   }
 });
