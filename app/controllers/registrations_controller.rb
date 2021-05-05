fb99c5f0bbd23f4bddfe988446dff9ef5630a818
Nate Eagle <nate.eagle@greymatter.io>
2021-05-05 17:39:18 -0400

    Modify registration to get values from Members API
    Make adding an attendee easier by searching the AGA Members API for
    their name or AGA ID and using the values there to pre-fill parts of
    their Congress registration.
    
    The new flow presents a starting screen after clicking "add attendee"
    that displays only a search box for the API and a button for anyone
    trying to register without being a member. (Many non-players and other
    guests are attendees at a typical Congress.)
    
    Once either a member is selected via the search or the not-a-member
    button is clicked, the next page is the previous registration form, but
    with as many values as I could find that were relevant filled in from
    existing info.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,55 @@ class RegistrationsController < ApplicationController
     attendee.year = @year.year
     @registration = Registration.new(current_user, attendee)
     expose_legacy_form_vars # TODO: don't!
+
+    if params.has_key?(:aga_id)
+      # Get member info from the API
+      apiUrl = "https://www.usgo.org/mm/api/members/"
+      action = "#{params[:aga_id]}?api_key=#{ENV['AGA_MEMBERS_API_KEY']}"
+
+      begin
+        buffer = open("#{apiUrl}#{action}").read
+        result = JSON.parse(buffer)
+
+        if result['success']
+          # Pre-fill attendee values with AGA Member Info
+          i = result['payload']['row']
+          attendee.given_name = i['given_names']
+          attendee.family_name = i['family_name']
+
+          # Grab the first letter of gender
+          # Ex: 'female' => 'f'
+          attendee.gender = i['gender'][0] rescue ''
+
+          attendee.birth_date = i['dob']
+          attendee.aga_id = i['member_id']
+          attendee.phone = i['phone']
+          attendee.local_phone = i['phone']
+          attendee.email = i['email']
+          attendee.state = i['state']
+
+          # Minimum rating is -30 (30k)
+          attendee.rank = [i['rating'].to_i, -30].max.to_s
+
+          if (i['country'] == 'USA')
+            attendee.country = 'US'
+          else
+            # Try to get the country code from the site constants
+            found = COUNTRIES.detect {|country| country[0] == i['country']}
+            if found
+              attendee.country = found[1]
+            end
+          end
+        end
+
+        rescue OpenURI::HTTPError
+          # No worries! We just won't pre-fill any values.
+      end
+
+      render :new
+    else
+      render :aga_member_search
+    end
   end
 
   def create
