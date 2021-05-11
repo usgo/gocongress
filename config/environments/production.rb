c618238ba18ba911b8d19a49a8e24934b42f6421
Nate Eagle <nate.eagle@greymatter.io>
2021-05-11 09:37:52 -0400

    Enable ES6 syntax for JavaScript files
    (Try to) fix deployment by enabling ES6 syntax in the uglifier.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.compress = true
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # enables the use of MD5 fingerprints in asset names
