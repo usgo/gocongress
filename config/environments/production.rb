76e3d3b5561940b7db7b4bb413583ba01a6e78b4
Nate Eagle <nate.eagle@greymatter.io>
2021-05-13 21:31:41 -0400

    Another attempt to fix asset serving in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   config.assets.digest = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
