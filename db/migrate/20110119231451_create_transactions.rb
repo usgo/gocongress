e2c5a5b6554cfa02e69f81b7eeecf72ee8822ac5
Jared Beck <jared@jaredbeck.com>
2021-06-19 21:18:35 -0400

    Layout/ExtraSpacing

--- db/migrate/20110119231451_create_transactions.rb
+++ db/migrate/20110119231451_create_transactions.rb
@@ -1,10 +1,10 @@
 class CreateTransactions < ActiveRecord::Migration
   def self.up
     create_table :transactions do |t|
-      t.integer :userid,				:null => false
-      t.string :trantype,				:null => false, :limit => 1
-      t.decimal :amount,				:null => false, :scale => 2, :precision => 10
-      t.integer :gwtranid,			:null => false
+      t.integer :userid,	:null => false
+      t.string :trantype,	:null => false, :limit => 1
+      t.decimal :amount,	:null => false, :scale => 2, :precision => 10
+      t.integer :gwtranid,	:null => false
       t.timestamp :gwtimestamp, :null => false
 
       t.timestamps
