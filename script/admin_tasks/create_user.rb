143fd29081c8ea815f698192fa6b68554e770353
Nate Eagle <nate.eagle@greymatter.io>
2021-06-20 12:32:46 -0400

    Add prompts to create_user script
    Make the create_user script interactive to make it easier for developers
    to create users without needing to make any modifications to files which
    would then show up as changes in their git status.

--- script/admin_tasks/create_user.rb
+++ script/admin_tasks/create_user.rb
@@ -1,11 +1,35 @@
-# Create new users for local development
 require 'colorize'
+require 'tty-prompt'
+prompt = TTY::Prompt.new
+
+# Create a new user for local development
+
 u = User.new
-u.email	= "admin@localhost.local"
-u.password = "foofighters"
-u.password_confirmation	= "foofighters"
-u.year = 2021
+u.email = prompt.ask("New user's email:") do |q|
+  q.validate(URI::MailTo::EMAIL_REGEXP, "Invalid email address")
+end
+
+u.password = prompt.mask("Create a password:") do |q|
+  q.validate(proc { |p| p.length >= 6 })
+  q.messages[:valid?] = "Password must be 6 characters or longer"
+end
+
+u.password_confirmation = prompt.mask("Confirm password:") do |q|
+  q.validate(proc { |p| p == u.password })
+  q.messages[:valid?] = "Passwords don't match!"
+end
+
+u.year = prompt.ask("Year:", default: DateTime.now.year)
+
+# Pretend we've confirmed the new user's email
 u.confirmed_at = DateTime.now
-u.role = 'A'
-u.save!
-puts "New user created!".green
+
+roles = { Admin: 'A', Staff: 'S', User: 'U' }
+u.role = prompt.select("User role:", roles)
+
+if u.valid?
+  u.save!
+  puts "New user created!".cyan
+else
+  u.errors.each { |k, v| puts "#{k.capitalize}: #{v}".red }
+end
