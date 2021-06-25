d557158d96c9eef93dfc1c81fb06f59408df140b
Nate Eagle <nate.eagle@greymatter.io>
2021-06-25 10:29:10 -0400

    Restore XML file format and AGA info
    This restores the logic necessary to generate the XML files for player
    list imports to OpenGotha. I also restored the information from the AGA
    TD List. We'll want to refactor that part of the site to use info from
    the Membership API instead of manually scraping the TD list, but it can
    stay as-is for now.

--- app/controllers/rpt/tournament_reports_controller.rb
+++ app/controllers/rpt/tournament_reports_controller.rb
@@ -6,5 +6,22 @@ class Rpt::TournamentReportsController < Rpt::AbstractReportController
     @page_title = @tournament.name
 
     @players = @tournament.attendees
+
+    @aga_member_info = AgaTdList.data
+
+    respond_to do |format|
+      format.html do
+        @players_count = @players.count
+        render :show
+      end
+      format.xml do
+        xml = PlayersXmlExporter.render(@players, @aga_member_info)
+        send_data xml, filename: xml_filename, type: 'text/xml'
+      end
+    end
+  end
+
+  def xml_filename
+    "#{helpers.slugify(@tournament.name, '_')}_players_#{Time.current.strftime('%Y-%m-%d')}.xml"
   end
 end
