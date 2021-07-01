71934939759efa68aa579474753840cc59038bf4
Nate Eagle <nate.eagle@greymatter.io>
2021-06-30 20:15:25 -0400

    Teach NumberHelper about zeroes

--- app/helpers/number_helper.rb
+++ app/helpers/number_helper.rb
@@ -3,7 +3,7 @@
 module NumberHelper
   # @return String
   def usgc_number(i)
-    if i.is_a?(Integer) && (1..9).cover?(i)
+    if i.is_a?(Integer) && (0..9).cover?(i)
       t(format('gocongress.number.%d', i))
     else
       i.to_s
@@ -16,9 +16,13 @@ module NumberHelper
   # @example usgc_pluralize(10, 'user') # "10 users"
   # @example usgc_pluralize(1.3, 'banana') # "1.3 bananas"
   # @return String
-  def usgc_pluralize(i, singular)
+  def usgc_pluralize(i, singular = nil)
     int = i.is_a?(Integer)
-    noun = i > 1 || !int ? singular.pluralize(I18n.locale) : singular
-    format('%s %s', usgc_number(i), noun)
+    if singular
+      noun = i != 1 || !int ? singular.pluralize(I18n.locale) : singular
+      return format('%s %s', usgc_number(i), noun)
+    else
+      return usgc_number(i)
+    end
   end
 end
