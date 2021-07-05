a8cd5c6b1652fbb36f5ef7d4c7525dd9784a7164
Michael Hiiva <michaelhiiva@gmail.com>
2021-07-04 20:47:38 -0700

    Small fix to remove container after creation.

--- script/admin_tasks/create_user_with_docker.sh
+++ script/admin_tasks/create_user_with_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 # Create a user using the create_user.rb script but using a docker run to create this within the app_database.
-docker-compose run app bash -c "rails db:environment:set RAILS_ENV=development && rake db:setup && rails runner script/admin_tasks/create_user.rb"
+docker-compose run app --rm bash -c "rails db:environment:set RAILS_ENV=development && rake db:setup && rails runner script/admin_tasks/create_user.rb"
