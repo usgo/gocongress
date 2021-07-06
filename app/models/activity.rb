7d214f3381ddc7860ef68b85006e960a92a4f324
Jared Beck <jared@jaredbeck.com>
2021-07-06 00:50:10 -0400

    Test validation of activities.price
    Just making sure rails merges the two `numericality` validations.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -10,6 +10,8 @@ class Activity < ApplicationRecord
   validates_presence_of :leave_time, :name, :return_time
   validates :location, :length => { :maximum => 50 }
   validates :phone, :length => { :maximum => 20 }
+
+  # See additional `numericality` validations of `price` in `Purchasable`.
   validates :price, :numericality => {
     :equal_to => 0,
     :if => :price_varies?,
@@ -17,6 +19,7 @@ class Activity < ApplicationRecord
       set the price to 0, so that this #{model_name.human.downcase}
       will not show up on invoices."
   }
+
   validates :url,
     :length => { :maximum => 200 },
     :format => {
