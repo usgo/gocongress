a3e8815dad93edaea21ef30ce593cf7f1e390861
Jared Beck <jared@jaredbeck.com>
2021-07-06 00:14:51 -0400

    Lint/EmptyClass

--- app/models/sms_notification.rb
+++ app/models/sms_notification.rb
@@ -1,2 +1,15 @@
 class SmsNotification
+  def initialize(*)
+    ActiveSupport::Deprecation.warn(
+      'Suspected defunct class SmsNotification received #initialize'
+    )
+    super
+  end
+
+  def save(*)
+    ActiveSupport::Deprecation.warn(
+      'Suspected defunct class SmsNotification received #save'
+    )
+    super
+  end
 end
