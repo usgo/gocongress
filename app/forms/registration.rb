usgo/gocongress

commit 5f1195b0e5b40db45e2d6e69a58570f84684bd41

Improve attendee count by filtering out canceled attendees
At some point soon, I need to change all the instances of "cancelled"
with two ls to "canceled". Using two Ls is not actually incorrect, both
are accepted variants, but two Ls is much more common in British
English. Since this is the US Go Congress, I think it's better to use
one L, but having both in the code base at once is definitely less than
ideal.

I don't want to do a migration on the field name right before Congress,
however.

app/forms/registration.rb
@@ -103,7 +103,7 @@ class Registration
   end
 
   def attendee_number
-    @attendee.user.attendees.count + 1
+    @attendee.user.uncanceled_attendees.length
   end
 
   private

app/models/user.rb
@@ -123,6 +123,10 @@ class User < ApplicationRecord
     ROLES.select { |r| r[1] == role }.first[0]
   end
 
+  def uncanceled_attendees
+    attendees.reject { |attendee| attendee.cancelled }
+  end
+
   # Override the built-in devise method update_with_password()
   # so that we don't need current_password
   # Credit: Carlos Antonio da Silva

app/views/terminus/show.haml
@@ -1,7 +1,7 @@
 %h2 What next?
 %p
   You have
-  = pluralize(@user.attendees.count, 'attendee')
+  = pluralize(@user.uncanceled_attendees.length, 'attendee')
   on your account.
   
   - if @user.balance > 0

app/views/users/index.html.haml
@@ -17,7 +17,7 @@
   %strong= usgc_pluralize(@users.length, 'user')
   registered, with
   %strong
-    - attendees = @users.reduce(0) { |sum, user| sum + user.attendees.select {|attendee| !attendee.cancelled}.length}
+    - attendees = @users.reduce(0) { |sum, user| sum + user.uncanceled_attendees.length}
     = usgc_pluralize(attendees, 'attendee') + '.'
   %strong
     - incomplete = @users.reduce(0) { |sum, user| user.attendees.length > 0 ? sum : sum + 1 }

commit 135e26c0dbfdcb7fc518907c3ccea0acd7b91fb4

Fix Registration#attendee_number
The definition was changed in 5f1195b0e5b40db45e2d6e69a58570f84684bd41,
which broke two tests, e.g. registrations_controller_spec.rb:73

I believe the change was incorrect, as explained in my comment. So,
this commit reverts that change. This is a *partial* reversion of
5f1195b0e5b40db45e2d6e69a58570f84684bd41. The other parts are not
familiar to me (I don't know what "canceling" an attendee means)
so I left them as is.

app/forms/registration.rb
@@ -102,8 +102,11 @@ class Registration
     form_plans.group_by(&:plan_category)
   end
 
+  # At the top of `registrations/new`, we say "First Attendee",
+  # "Second Attendee", etc. If you are filling out the form for your second
+  # attendee, it should say "Second Attendee".
   def attendee_number
-    @attendee.user.uncanceled_attendees.length
+    @attendee.user.uncanceled_attendees.length + 1
   end
 
   private