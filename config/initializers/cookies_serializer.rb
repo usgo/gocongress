c0de9d15abdce2877f60a9ecace1dc5ef65b2dc5
Jared Beck <jared@jaredbeck.com>
2022-04-11 00:49:23 -0400

    action_dispatch.cookies_serializer = :hybrid

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -2,4 +2,15 @@
 
 # Specify a serializer for the signed and encrypted cookie jars.
 # Valid options are :json, :marshal, and :hybrid.
-Rails.application.config.action_dispatch.cookies_serializer = :json
+#
+# When I upgraded to Rails 6.1 I noticed the option to switch to `:json`.
+# We were using `:marshal` before. Apparently, that's a security vulnerability.
+#
+# But, when I switched to `:json`, I got a nasty error parsing my old marshaled
+# cookie.
+#
+# > ActionView::Template::Error (809: unexpected token at {I"session_id:ETI"%8792a9....
+#
+# This setting is not documented well, but it seems `:hybrid` will detect
+# marshaled cookies and switch them to JSON.
+Rails.application.config.action_dispatch.cookies_serializer = :hybrid
